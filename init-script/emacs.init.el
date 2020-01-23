c3f7eb7726c14cb51b780d72be4ba07a19f62539
gadmyth <gadmyth@gmail.com>
2020-01-23 18:24:59 +0800

    optimize with new 'version compare func'

--- init-script/emacs.init.el
+++ init-script/emacs.init.el
@@ -1,3 +1,11 @@
-(if (string< (emacs-version) "24")
+;;; package --- emacs.init.el
+;;; Commentary:
+;;; Code:
+
+(if (version< emacs-version "24")
     (load-file (expand-file-name "~/.emacs.shell.el"))
   (load-file (expand-file-name "~/.emacs.frame.el")))
+
+(provide 'emacs.init)
+;;; emacs.init.el ends here
+
