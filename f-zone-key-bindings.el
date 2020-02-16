c213307ccf85bc91c9542fb053b11c83ee53dad1
gadmyth <gadmyth@gmail.com>
2020-02-16 11:17:25 +0800

    rename key-bindings to f-zone-key-bindings, refactor bindings

--- f-zone-key-bindings.el
+++ f-zone-key-bindings.el
@@ -1,23 +1,31 @@
-;;; package --- key-bindings.el
+;;; package --- f-zone-key-bindings.el
 ;;; Commentary:
 ;;; Code:
 
+
 (require 'eyebrowse-config)
 (require 'customized-dir)
-
-;;; F zone
-(global-set-key (kbd "<f1>") 'switch-buffer)
-(global-set-key (kbd "<M-f1>") 'switch-to-customized-dir)
-
-(global-set-key (kbd "<f2>") 'eyebrowse-list-configs)
-(global-set-key (kbd "<M-f2>") '(lambda () (interactive) (switch-to-buffer (other-buffer))))
-
-(global-set-key (kbd "<f3>") 'eyebrowse-list-actions)
-(global-set-key (kbd "<M-f3>") 'eyebrowse-last-window-config)
-
 (require 'ivy)
-(global-set-key (kbd "<f4>") 'eyebrowse--ivy-switch-buffer)
-(global-set-key (kbd "<M-f4>") 'new-buffer)
+
+;; buffer
+(global-set-key (kbd "<f1>") 'switch-buffer-default-scratch)
+(global-set-key (kbd "<M-f1>") 'new-buffer)
+
+;; eyebrowse config
+(global-set-key (kbd "<f2>") 'eyebrowse-list-configs)
+(global-set-key (kbd "<M-f2>") 'eyebrowse-modify-config)
+(global-set-key (kbd "<C-f2>") 'eyebrowse-last-window-config)
+
+;; eyebrowse buffer config
+(global-set-key (kbd "<f3>") 'eyebrowse--ivy-switch-buffer)
+(global-set-key (kbd "<M-f3>") 'eyebrowse-modify-buffer-config)
+(global-set-key (kbd "<C-f3>") 'eyebrowse-switch-other-buffer)
+
+;; customized dir
+(global-set-key (kbd "<f4>") 'switch-to-customized-dir)
+;; *** not implemented ***
+(global-set-key (kbd "<C-f4>") 'cusomized-dir-action)
+
 
 (global-set-key (kbd "<f5>") '(lambda () (interactive)
                                 (let ((index (string-match "\\(.*\\)\\.\\(.\\)" (buffer-name)))
@@ -29,9 +37,9 @@
                                       (if (equal "h" suffix)
                                           (find-file (concatenate 'string prename ".m"))))))))
 
+;; library
 (global-set-key (kbd "<f6>") 'find-library)
 (global-set-key (kbd "<M-f6>") 'require-library)
 
-
-(provide 'key-bindings)
-;;; key-bindings.el ends here
+(provide 'f-zone-key-bindings)
+;;; f-zone-key-bindings.el ends here
