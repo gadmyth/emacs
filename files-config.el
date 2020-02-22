453616f523a593c921b6e0d2db3deac116fb7a6c
gadmyth <gadmyth@gmail.com>
2020-02-22 22:43:44 +0800

    revert buffer to default coding system

--- files-config.el
+++ files-config.el
@@ -4,14 +4,12 @@
 
 (add-hook 'find-file-hook
           (lambda ()
-            (progn
-              (interactive)
-              (revert-buffer t t t)
-              (scale-large)
-              (require-if-installed 'textmate (textmate-mode))
-              (require-if-installed 'xcscope (cscope-minor-mode))
-              (display-line-numbers-mode (if (equal major-mode 'org-mode) 0 1))
-              )))
+            (revert-buffer-with-default-coding-system)
+            (scale-large)
+            (require-if-installed 'textmate (textmate-mode))
+            (require-if-installed 'xcscope (cscope-minor-mode))
+            (display-line-numbers-mode (if (equal major-mode 'org-mode) 0 1))
+            ))
 
 (add-hook 'after-save-hook
           (lambda () (if (string= (buffer-name) ".emacs")
@@ -20,6 +18,12 @@
 (add-hook 'after-init-hook 'global-flycheck-mode)
 
 (setq backup-directory-alist (quote (("." . "~/.backups"))))
+
+(defun revert-buffer-with-default-coding-system ()
+  "."
+  (let ((coding-system-for-read default-buffer-file-coding-system))
+    (revert-buffer t t t)))
+
 
 (provide 'files-config)
 ;;; files-config.el ends here
