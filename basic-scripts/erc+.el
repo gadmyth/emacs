23d2a3a00e23c2f4d9dc411dd95d11290f9c4399
gadmyth <gadmyth@gmail.com>
2020-03-24 19:01:38 +0800

    erc: mv erc extension function to erc+.el

---
+++ basic-scripts/erc+.el
@@ -0,0 +1,252 @@
+;;; erc+.el --- Package.  -*- lexical-binding: nil; -*-
+
+;; Copyright (C) 2020 gadmyth
+
+;; Author: erc+.el <gadmyth@gmail.com}>
+;; Version: 1.0
+;; Package-Version: 20200324.001
+;; Package-Requires: erc, s, text-mode
+;; Keywords: erc+.el
+;; Homepage: https://www.github.com/gadmyth/emacs
+;; URL: https://www.github.com/gadmyth/emacs/blob/master/basic-scripts/erc+.el
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Source code
+;;
+;; erc+'s code can be found here:
+;;   https://www.github.com/gadmyth/emacs/blob/master/basic-scripts/erc+.el
+
+;;; Commentary:
+;;; Code:
+
+(require 'erc)
+(require 's)
+(require 'text-mode)
+
+
+(defvar erc-nick nil)
+(defvar *erc-debug* nil)
+(defvar *erc-aggregate-buffer* nil)
+(defvar erc-aggregate-refresh t)
+
+(defmacro erc-message (format-string &rest ARGS)
+  "If debug is open, send message with FORMAT-STRING and ARGS."
+  `(if *erc-debug*
+       (message ,format-string ,@ARGS)))
+
+(defun erc-aggregate-toggle-refresh ()
+  "."
+  (interactive)
+  (setq erc-aggregate-refresh (not erc-aggregate-refresh))
+  (message "turn %s the erc refresh." (if erc-aggregate-refresh "on" "off")))
+
+;; **** add a extra function to erc-server-PRIVMSG-functions hook ****
+(add-hook 'erc-server-PRIVMSG-functions #'erc-server-PRIVMSG_AGGREGATE t)
+
+(defun erc-server-PRIVMSG_AGGREGATE (proc parsed)
+  "Handle private messages, including messages in channels.
+With PARSED message and PROC."
+  (let* ((sender-spec (erc-response.sender parsed))
+         (cmd (erc-response.command parsed))
+         (tgt (car (erc-response.command-args parsed)))
+         (msg (erc-response.contents parsed))
+         (short-sender (erc-short-sender sender-spec)))
+    (erc-update-aggregate-buffer short-sender tgt msg)
+    (erc-message "erc message: %s, %s: %s" short-sender tgt msg)
+    ;; return nil, to exec the next function in the hook's list
+    nil))
+
+(defun erc-short-sender (sender)
+  "Get short sender of the SENDER of erc-response.sender."
+  (if-let ((index (s-index-of "!" sender)))
+      (substring sender 0 index)
+    sender))
+
+(defun erc-sender-with-group (sender target)
+  "Get sender and group of the SENDER and TARGET."
+  (if (string-prefix-p "#" target)
+      (let ((group-name (substring tgt 1 (length target))))
+        (format "%s@%s" sender group-name))
+    sender))
+
+
+(defvar *wechat-multi-message-table* (make-hash-table :test 'equal))
+
+(defun erc-parse-wechat-share-msg (sender msg)
+  "Parse and store wechat multi share MSG and SENDER."
+  (cond
+   ((string-prefix-p "[应用分享]" msg)
+    (let ((table *wechat-multi-message-table*)
+          (title-key (concat sender "_title"))
+          (desc-key (concat sender "_description"))
+          (app-key (concat sender "_app"))
+          (link-key (concat sender "_link")))
+      ;; record the wechat share msg
+      (cond ((s-contains-p "标题：" msg)
+             (puthash title-key (substring msg 9) table))
+            ((s-contains-p "描述：" msg)
+             (puthash desc-key (substring msg 9) table))
+            ((s-contains-p "应用：" msg)
+             (puthash app-key (substring msg 9) table))
+            ((s-contains-p "链接：" msg)
+             (puthash link-key (substring msg 9) table)))
+      ;; merge 4 msg to one
+      (cond ((and (gethash title-key table)
+                  (gethash desc-key table)
+                  (gethash app-key table)
+                  (gethash link-key table))
+             (setq msg (format "[%s]\n%s\n%s"
+                               (gethash title-key table)
+                               (gethash desc-key table)
+                               (gethash link-key table)))
+             (remhash title-key table)
+             (remhash desc-key table)
+             (remhash app-key table)
+             (remhash link-key table)
+             msg)
+            (t
+             nil))))
+   ;; msg is not a wechat share msg
+   (t msg)))
+
+(defun erc-parse-wechat-file-msg (msg)
+  "Parse MSG if it's an file message."
+  (when (string-match "\\[\\(.*\\)\\](\\(.*\\))" msg)
+    (let ((indicator
+           (substring msg (match-beginning 1) (match-end 1)))
+          (file-path
+           (substring msg (match-beginning 2) (match-end 2))))
+      (cons indicator file-path))))
+
+(defmacro erc-save-excursion (&rest body)
+  "Eval the BODY using 'save-excursion or not by SAVE-EXCURSION-P."
+  `(cond (,erc-aggregate-refresh
+          (message "no save excursion")
+         ,@body)
+        (t
+         (save-excursion
+           (message "save excursion")
+           ,@body))))
+
+(defun* erc-update-aggregate-buffer (short-sender target msg)
+  "Append SENDER and MSG to the *erc-aggregate-buffer*."
+  (let ((sender (erc-sender-with-group short-sender target))
+        erc-msg-link erc-link-start
+        msg-file-path
+        msg-picture-p)
+    (if-let ((wechat-msg (erc-parse-wechat-share-msg sender msg)))
+        (setq msg wechat-msg)
+      (return-from erc-update-aggregate-buffer))
+
+    ;; parse the wechat file msg as picture, file or emoji
+    (when-let ((file-msg (erc-parse-wechat-file-msg msg)))
+      (setq msg (car file-msg))
+      (setq msg-file-path (cdr file-msg))
+      (when (or (string-equal "图片" (car file-msg))
+                (string-equal "表情" (car file-msg)))
+        (setq msg "")
+        (setq msg-picture-p t)))
+  
+    (when (not (buffer-live-p *erc-aggregate-buffer*))
+      ;; create buffer if not exists
+      (setq *erc-aggregate-buffer*
+            (generate-new-buffer "*erc-aggregate-buffer*"))
+      ;; set the major mode
+      (with-current-buffer *erc-aggregate-buffer*
+        (erc-aggregate-mode)))
+    ;; append msg
+    (with-current-buffer *erc-aggregate-buffer*
+      (erc-save-excursion
+       (goto-char (point-max))
+       (let* ((now (format-time-string "%Y-%m-%d %a %H:%M:%S" (current-time)))
+              (content (format "%s [%s]:\n%s\n\n" sender now msg))
+              (new-msg-start (point-max))
+              (time-start (- (s-index-of now content) 1))
+              (time-end (+ time-start (length now) 2))
+              (msg-start (+ time-end 2))
+              (msg-end (+ msg-start (length msg))))
+         (erc-put-text-property time-start time-end
+                                'font-lock-face 'erc-notice-face content)
+         ;; font lock the link
+         (when-let ((index (string-match "\\(https?://[^\s]*\\)" msg)))
+           (setq erc-link-start index)
+           (setq erc-msg-link (substring msg (match-beginning 1) (match-end 1))))
+         (insert content)
+         ;; insert nick button
+         (erc-button-add-button new-msg-start (+ new-msg-start (length sender))
+                                #'erc-button-nick-callback
+                                nil (list (cons short-sender target)))
+         ;; insert file or picture
+         (when msg-file-path
+           (cond (msg-picture-p
+                  (insert-image (create-image msg-file-path nil nil))
+                  (insert "\n"))
+                 (t
+                  (erc-button-add-button (+ new-msg-start msg-start) (+ new-msg-start msg-end)
+                                         #'erc-button-file-callback
+                                         nil (list msg-file-path)))))
+         ;; insert link button
+         (when erc-msg-link
+           (erc-button-add-button (+ new-msg-start msg-start erc-link-start) (+ new-msg-start erc-link-start msg-end)
+                                  #'erc-button-link-callback
+                                  nil (list erc-msg-link))
+           (setq erc-msg-link nil))
+         (set-window-point (get-buffer-window *erc-aggregate-buffer*) (point-max)))))
+    ;; show window
+    (when erc-aggregate-refresh
+      (display-buffer *erc-aggregate-buffer*))))
+
+(defun erc-button-nick-callback (data)
+  "When click the nick name erc-button, response with the DATA."
+  (let* ((short-sender (car data))
+         (target (cdr data))
+         (channel (and (string-prefix-p "#" target) target))
+         (buffer-name (or channel
+                          (and (string-equal target erc-nick) short-sender)
+                          target)))
+    (erc-message "nick click, data: %s, short-sender: %s, channel: %s, buffer-name: %s" data short-sender channel buffer-name)
+    (let* ((buffer (get-buffer buffer-name))
+           (window (get-buffer-window buffer)))
+      (display-buffer buffer)
+      (if (and window (window-live-p window))
+          (select-window window)))))
+
+(defun erc-button-file-callback (data)
+  "When click the nick name erc-button, response with the DATA."
+  ;; file(open-file), picture(show)
+  (message "file path: %s" data)
+  (if (not (fboundp 'open-file-by-system))
+      (require 'system-util))
+  (apply #'open-file-by-system (list data)))
+
+(defun erc-button-link-callback (data)
+  "When click the nick name erc-button, response with the DATA to open the link."
+  (browse-url data))
+
+
+(define-derived-mode erc-aggregate-mode text-mode "ErcA"
+  ;; The mode for *erc-aggregate-buffer*.
+  )
+
+(defvar erc-aggregate-mode-map
+  (let ((map (make-sparse-keymap)))
+    (set-keymap-parent erc-aggregate-mode-map text-mode-map)
+    (define-key map "q" 'quit-window)
+    map))
+
+(provide 'erc+)
+;;; erc+.el ends here
--- basic-scripts/irc-config.el
+++ basic-scripts/irc-config.el
@@ -2,35 +2,14 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 'erc)
-(require 's)
-(require 'text-mode)
+(require 'erc+)
 
-(define-derived-mode erc-aggregate-mode text-mode "ErcA"
-  ;; The mode for *erc-aggregate-buffer*.
-  )
-
-(defvar erc-aggregate-mode-map
-  (let ((map (make-sparse-keymap)))
-    (set-keymap-parent erc-aggregate-mode-map text-mode-map)
-    (define-key map "q" 'quit-window)
-    map))
-
-(defvar erc-nick nil)
 (defvar erc-hide-list '("JOIN" "QUIT" "PART" "NICK" "MODE"))
-(defvar *erc-aggregate-buffer* nil)
 
-(defface erc-time-face
-  '((default :weight bold :foreground "Green")
-    (t :foreground "green"))
-  "ERC face for time."
-  :group 'erc-faces)
+(setq erc-server-coding-system '(utf-8-unix . utf-8-unix))
 
-(defface erc-link-face
-  '((default :underline t :foreground "SlateBlue")
-    (t :foreground "blue"))
-  "ERC face for link."
-  :group 'erc-faces)
+(defun erc-setup-buffer (buffer)
+  "Override the erc-setup-buffer function, don't display the BUFFER automatically.")
 
 (defun start-erc (server port nick-name passwd)
   "Start erc with NICK-NAME and PASSWD of erc SERVER at PORT."
@@ -40,129 +19,6 @@
                 (read-string "nick name: " erc-nick)
                 (read-passwd "passwd: ")))
   (erc :server server :port port :nick nick-name :password passwd))
-
-;; **** define the autojoin channels ****
-;(require 'erc-join)
-;(setq erc-autojoin-channels-alist '(("freenode.net" "#wecase" "#emacs")))
-
-
-;; **** add a extra function to erc-server-PRIVMSG-functions hook ****
-(add-hook 'erc-server-PRIVMSG-functions #'erc-server-PRIVMSG_AGGREGATE t)
-
-(defun erc-server-PRIVMSG_AGGREGATE (proc parsed)
-  "Handle private messages, including messages in channels.
-With PARSED message and PROC."
-  (let* ((sender-spec (erc-response.sender parsed))
-         (cmd (erc-response.command parsed))
-         (tgt (car (erc-response.command-args parsed)))
-         (msg (erc-response.contents parsed))
-         (short-sender (erc-short-sender sender-spec))
-         (sender (erc-sender-with-group short-sender tgt)))
-    (erc-update-aggregate-buffer sender msg)
-    (message "erc message: %s: %s" sender msg)
-    ;; return nil, to exec the next function in the hook's list
-    nil))
-
-(defun erc-short-sender (sender)
-  "Get short sender of the SENDER of erc-response.sender."
-  (if-let ((index (s-index-of "!" sender)))
-      (substring sender 0 index)
-    sender))
-
-(defun erc-sender-with-group (sender target)
-  "Get sender and group of the SENDER and TARGET."
-  (if (string-prefix-p "#" target)
-      (let ((group-name (substring tgt 1 (length target))))
-        (format "%s@%s" sender group-name))
-    sender))
-
-(defvar *wechat-multi-message-table* (make-hash-table :test 'equal))
-
-(defun erc-parse-wechat-share-msg (sender msg)
-  "Parse and store wechat multi share MSG and SENDER."
-  (cond
-   ((string-prefix-p "[应用分享]" msg)
-    (let ((table *wechat-multi-message-table*)
-          (title-key (concat sender "_title"))
-          (desc-key (concat sender "_description"))
-          (app-key (concat sender "_app"))
-          (link-key (concat sender "_link")))
-      ;; record the wechat share msg
-      (cond ((s-contains-p "标题：" msg)
-             (puthash title-key (substring msg 9) table))
-            ((s-contains-p "描述：" msg)
-             (puthash desc-key (substring msg 9) table))
-            ((s-contains-p "应用：" msg)
-             (puthash app-key (substring msg 9) table))
-            ((s-contains-p "链接：" msg)
-             (puthash link-key (substring msg 9) table)))
-      ;; merge 4 msg to one
-      (cond ((and (gethash title-key table)
-                  (gethash desc-key table)
-                  (gethash app-key table)
-                  (gethash link-key table))
-             (setq msg (format "[%s]\n%s\n%s"
-                               (gethash title-key table)
-                               (gethash desc-key table)
-                               (gethash link-key table)))
-             (remhash title-key table)
-             (remhash desc-key table)
-             (remhash app-key table)
-             (remhash link-key table)
-             msg)
-            (t
-             nil))))
-   ;; msg is not a wechat share msg
-   (t msg)))
-
-(defun* erc-update-aggregate-buffer (sender msg)
-  "Append SENDER and MSG to the *erc-aggregate-buffer*."
-  (if-let ((wechat-msg (erc-parse-wechat-share-msg sender msg)))
-      (setq msg wechat-msg)
-    (return-from erc-update-aggregate-buffer))
-  
-  (when (not (buffer-live-p *erc-aggregate-buffer*))
-    ;; create buffer if not exists
-    (setq *erc-aggregate-buffer*
-          (generate-new-buffer "*erc-aggregate-buffer*"))
-    ;; set the major mode
-    (with-current-buffer *erc-aggregate-buffer*
-      (erc-aggregate-mode)))
-  ;; append msg
-  (with-current-buffer *erc-aggregate-buffer*
-    (goto-char (point-max))
-    (let* ((now (format-time-string "%Y-%m-%d %a %H:%M:%S" (current-time)))
-           (content (format "%s [%s]:\n%s\n\n" sender now msg))
-           (new-msg-start (point-max))
-           (time-start (- (s-index-of now content) 1))
-           (time-end (+ time-start (length now) 2))
-           (msg-start (+ time-end 2))
-           (msg-end (+ msg-start (length msg))))
-      ;(erc-put-text-property 0 (length sender)
-      ;                       'font-lock-face 'erc-nick-default-face content)
-      (erc-put-text-property time-start time-end
-                             'font-lock-face 'erc-notice-face content)
-      ;; font lock the link
-      (when-let ((index (string-match "http" msg)))
-        (erc-put-text-property  (+ msg-start index) msg-end
-                                'font-lock-face 'erc-link-face content))
-      (insert content)
-      (erc-button-add-button new-msg-start (+ new-msg-start (length sender))
-                             #'erc-button-nick-callback
-                             nil (list sender))
-      (set-window-point (get-buffer-window *erc-aggregate-buffer*) (point-max))))
-  ;; show window
-  (display-buffer *erc-aggregate-buffer*))
-
-(defun erc-button-nick-callback (data)
-  "When click the nick name erc-button, response with the DATA."
-  (let* ((group-start (s-index-of "@" data))
-         (channel (and group-start (concat "#" (substring data (1+ group-start))))))
-    (let* ((buffer (get-buffer channel))
-           (window (get-buffer-window buffer)))
-      (display-buffer buffer)
-      (if (and window (window-live-p window))
-          (select-window window)))))
 
 (provide 'irc-config)
 ;;; irc-config.el ends here
