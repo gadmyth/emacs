9a8b3573fc6d1cc0b941e7ea1dbbdf8d88db9c8f
gadmyth <gadmyth@gmail.com>
2020-07-19 11:57:09 +0800

    add system shell's alias into eshell's alias

--- basic-scripts/shells.el
+++ basic-scripts/shells.el
@@ -2,16 +2,45 @@
 ;;; Commentary:
 ;;; Code:
 
+(defconst +LOAD-SHELL-PROFILE+ (if (eq window-system 'ns)
+                                   "source ~/.profile; source ~/mybash_profile"
+                                 "source ~/.bashrc"))
 
-(let ((path (if (eq window-system 'ns)
-                (shell-command-to-string "source ~/.profile; source ~/mybash_profile; echo $PATH")
-              (shell-command-to-string "source ~/.bashrc; echo $PATH"))))
+(let* ((command (format "%s; echo $PATH" +LOAD-SHELL-PROFILE+))
+       (path (shell-command-to-string command)))
   (setenv "PATH" path)
   (setq exec-path (append (split-string-and-unquote path ":") exec-path)))
 
 ;; The following will cause the bash problem: "bash: shell_session_update: command not found"
 ;; (setq shell-command-switch "-ic")
 
+
+(require 'eshell)
+(require 'em-alias)
+
+(defun eshell-load-bash-aliases ()
+  "Read bash aliases from Bash and insert them into the list of eshell aliases."
+  (interactive)
+  (setq eshell-command-aliases-list nil)
+  (let* ((command (format "%s; alias" +LOAD-SHELL-PROFILE+))
+         (shell-alias (shell-command-to-string command))
+         alias-array alias-list)
+    (setq shell-alias (replace-regexp-in-string "alias " "" shell-alias))
+    (setq shell-alias (replace-regexp-in-string "='" " " shell-alias))
+    (setq shell-alias (replace-regexp-in-string "'\n" "\n" shell-alias))
+    (setq shell-alias (replace-regexp-in-string "=\"" " " shell-alias))
+    (setq shell-alias (replace-regexp-in-string "\"\n" "\n" shell-alias))
+    (setq alias-array (split-string shell-alias "\n"))
+    (dolist (alias-line alias-array)
+      (when (string-match "\\(.*?\\) \\(.*\\)" alias-line)
+        (let ((alias-name
+               (substring alias-line (match-beginning 1) (match-end 1)))
+              (alias-command
+               (substring alias-line (match-beginning 2) (match-end 2))))
+          (push (list alias-name alias-command) alias-list))))
+    (setq eshell-command-aliases-list alias-list)))
+
+(add-hook 'eshell-mode-hook 'eshell-load-bash-aliases)
+
 (provide 'shells)
 ;;; shells.el ends here
-
