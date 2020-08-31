e662e85dc08cf008a244de2256a020ea300cdde8
gadmyth <gadmyth@gmail.com>
2020-08-31 21:29:02 +0800

    add lsp-config

---
+++ basic-scripts/lsp-config.el
@@ -0,0 +1,29 @@
+;;; package --- lsp-config.el
+;;; Commentary:
+;;; Code:
+
+
+(defvar +lsp-java-server-default-dir+ nil)
+
+(when +lsp-java-server-default-dir+
+  (require 'lsp)
+  (require 'lsp-ui)
+  (setq lsp-java-server-install-dir +lsp-java-server-default-dir+)
+
+  (setq lsp-ui-doc-enable nil)
+  (setq lsp-ui-sideline-enable nil)
+
+  (require 'lsp-java-boot)
+  (add-hook 'lsp-mode-hook #'lsp-lens-mode)
+  (add-hook 'java-mode-hook #'lsp-java-boot-lens-mode)
+
+  (require 'lsp-java)
+  (add-hook 'java-mode-hook #'lsp)
+
+  (require 'dap-java)
+  (require 'dap-mode)
+  (dap-auto-configure-mode)
+  )
+
+(provide 'lsp-config)
+;;; lsp-config.el ends here
