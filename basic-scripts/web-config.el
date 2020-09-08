9aad9c03aa9aaa97389506bc6bf01a060df92136
gadmyth <gadmyth@gmail.com>
2020-09-08 08:57:38 +0800

    add web-config

--- basic-scripts/web-config.el
+++ basic-scripts/web-config.el
@@ -12,17 +12,22 @@
         auto-mode-alist))
 
  (add-hook 'web-mode-hook
-           '(lambda () my-web-mode-indent-setup ()
-              (setq web-mode-markup-indent-offset 4) ; web-mode, html tag
-              (setq web-mode-css-indent-offset 4)    ; web-mode, css
-              (setq web-mode-code-indent-offset 4)   ; web-mode, js code
+           '(lambda ()
+              ; web-mode, html tag
+              (setq web-mode-markup-indent-offset 2)
+              ; web-mode, css
+              (setq web-mode-css-indent-offset 2)
+              ; web-mode, js code
+              (setq web-mode-code-indent-offset 2)
               ))
 
  (add-hook 'js2-mode-hook #'js2-refactor-mode)
  (js2r-add-keybindings-with-prefix "C-c C-m")
 
- (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
- (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+ ;; Auto-start on any markup modes
+ (add-hook 'sgml-mode-hook 'emmet-mode)
+ ;; enable Emmet's css abbreviation.
+ (add-hook 'css-mode-hook  'emmet-mode)
  )
 
 (provide 'web-config)
