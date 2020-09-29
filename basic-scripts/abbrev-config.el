7f03449eb9359da03921baff4665ffaa0927337a
gadmyth <gadmyth@gmail.com>
2020-09-30 00:26:02 +0800

    abbrev: fix bug: load abbrev's file has no effect

--- basic-scripts/abbrev-config.el
+++ basic-scripts/abbrev-config.el
@@ -4,18 +4,16 @@
 
 (require 'abbrev)
 
-(setq default-abbrev-mode t)
 (setq save-abbrevs t)
 
 (defun add-abbrev-hook (major-mode-name abbrev-alist)
   "When a MAJOR-MODE-NAME is loaded, define the abbrevs with ABBREV-ALIST in the major mode hook."
-  (let* ((hook-name (format "%s-hook" major-mode-name))
-         (hook-sym (intern hook-name)))
-    (add-hook hook-sym
-              (lambda()
-                (progn (mapc (lambda (pair)
-                               (define-abbrev objc-mode-abbrev-table (car pair) (cdr pair)))
-                             abbrev-alist))))))
+  (mapc (lambda (pair)
+  (let* ((table-name (format "%s-abbrev-table" major-mode-name))
+         (table-sym (intern table-name))
+         (table (symbol-value table-sym)))
+    (define-abbrev table (car pair) (cdr pair))))
+        abbrev-alist))
 
 (defvar *abbrev-default-directory* (expand-file-name "abbrevs" +emacs-context-directory+))
 
@@ -26,13 +24,13 @@
   "."
   (mapc (lambda (file-full-path)
           (let* ((relative-path (file-name-nondirectory file-full-path))
-                 (mode-name (file-name-sans-extension relative-path)))
+                 (major-mode-name (file-name-sans-extension relative-path)))
             (with-temp-buffer
               (insert-file-contents file-full-path)
               (goto-char (point-min))
               (let* ((content (read (current-buffer)))
                      (alist content))
-                (add-abbrev-hook mode-name alist)))))
+                (add-abbrev-hook major-mode-name alist)))))
         (directory-files *abbrev-default-directory* t ".*?\\.abbrev$")))
 
 (load-abbrevs)
