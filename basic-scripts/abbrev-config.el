2116596bc9709f6cddc1f3a9a314187aed21bccc
gadmyth <gadmyth@gmail.com>
2020-09-30 01:45:10 +0800

    abbrev: TAB bind to expand-abbrev first, or call the original binded function

--- basic-scripts/abbrev-config.el
+++ basic-scripts/abbrev-config.el
@@ -6,6 +6,24 @@
 
 (setq save-abbrevs t)
 
+(defvar abbrev-tab-functions nil)
+
+(defun tab-expand-abbrev-first ()
+  "."
+  (interactive)
+  (message "%S" (expand-abbrev))
+  (unless (expand-abbrev)
+    (when-let ((func (alist-get major-mode abbrev-tab-functions)))
+      (funcall func))))
+
+(defun bind-expand-abbrev-with-tab ()
+  "."
+  (let* ((key (kbd "TAB"))
+         (tab-function (key-binding key t)))
+    (when (not (function-equal tab-function #'tab-expand-abbrev-first))
+      (add-to-list 'abbrev-tab-functions (cons major-mode tab-function))
+      (local-set-key key #'tab-expand-abbrev-first))))
+
 (defun add-abbrev-hook (major-mode-name abbrev-alist)
   "When a MAJOR-MODE-NAME is loaded, define the abbrevs with ABBREV-ALIST in the major mode hook."
   (mapc (lambda (pair)
