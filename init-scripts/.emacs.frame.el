aeee6167140fd4108e67bf86b507685d0b7427b3
gadmyth <gadmyth@gmail.com>
2021-01-24 13:21:23 +0800

    wcy-desktop: wcy-desktop show load at last

--- init-scripts/.emacs.frame.el
+++ init-scripts/.emacs.frame.el
@@ -75,8 +75,8 @@
 (require-package 'yas-config (yas-global-mode))
 (require-package 'auto-complete (global-auto-complete-mode))
 (require-package 'customized-dir (customized-dir-init))
-(require-package 'wcy-desktop (wcy-desktop-init))
 (require-package 'eyebrowse+ (eyebrowse-plus-mode t))
+(require-package 'wcy-desktop (wcy-desktop-init))
 (require 'exwm-conf)
 
 (eval-after-load "textmate" '(add-to-list '*textmate-project-roots* ".svn"))
