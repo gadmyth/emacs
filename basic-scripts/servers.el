13f06f0426769fcc9951293a9cb45b6da972dd16
gadmyth <gadmyth@gmail.com>
2021-01-24 21:51:44 +0800

    servers: format the elisp code, remove TAB

--- basic-scripts/servers.el
+++ basic-scripts/servers.el
@@ -2,11 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 
+(require 'server)
+
 (if (and (not (eq window-system 'x))
-	   (or (not (boundp 'server-process))
-		   (null server-process)))
-	(ignore-errors
-	 (server-start)))
+         (or (not (boundp 'server-process))
+             (null server-process)))
+    (ignore-errors
+      (server-start)))
 
 (provide 'servers)
 ;;; servers.el ends here
