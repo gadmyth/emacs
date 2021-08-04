cc492983cfca30cedc08800af3a369a458b64792
gadmyth <gadmyth@gmail.com>
2021-08-04 22:45:45 +0800

    notifications: support send a notification at the certain time in the future

--- basic-scripts/notifications.el
+++ basic-scripts/notifications.el
@@ -3,12 +3,29 @@
 ;;; Code:
 
 (require 'async)
+(require 'dates)
 
-(defun start-notify (message time)
-  "Show MESSAGE as notification after TIME seconds."
-  (interactive "snotification to send: \nnafter seconds: ")
+(defun start-notify (message arg2)
+  "Show MESSAGE as notification after some minutes or at some certain time of ARG2."
+  (interactive
+   (list
+    (read-string "notification to send: " nil nil nil)
+    (if (null current-prefix-arg)
+        (read-number "after minitues: " 0)
+      (read-string "at the time: " (show-current-time-string)))))
+  (let ((minutes (if (null current-prefix-arg)
+                     arg2 (/ (- (string-to-number (string-to-timestamp arg2))
+                                (string-to-number (current-timestamp)))
+                             60.0))))
+    (when (> minutes 0)
+      (do-start-notify message minutes))))
+
+(defun do-start-notify (message minutes)
+  "Show MESSAGE as notification after some MINUTES."
   (async-start
-   `(lambda () (sleep-for ,time) ,time)
+   `(lambda ()
+      (let ((seconds (* 60 ,minutes)))
+        (sleep-for seconds) seconds))
    `(lambda (arg)
      (with-output-to-temp-buffer "*notification*"
        (print ,message)
