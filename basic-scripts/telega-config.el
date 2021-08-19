503297689dd923dec547bd6395ec71153a99c90a
gadmyth <gadmyth@gmail.com>
2021-08-19 13:46:43 +0800

    telega-config: fix bug: the socks5-proxy-server and socks5-proxy-port is not eval as value

--- basic-scripts/telega-config.el
+++ basic-scripts/telega-config.el
@@ -10,8 +10,8 @@
  (when (and *socks5-proxy-server* *socks5-proxy-port*)
    (setq telega-proxies
          (list
-          '(:server *socks5-proxy-server*
-                    :port *socks5-proxy-port*
+          `(:server ,*socks5-proxy-server*
+                    :port ,*socks5-proxy-port*
                     :enable t
                     :type (:@type "proxyTypeSocks5"))
           )))
