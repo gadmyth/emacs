34103b0fd23944d34145fa5b2ea428eb0d166532
gadmyth <gadmyth@gmail.com>
2021-09-13 11:19:05 +0800

    network-util: message the current-ip, public-ip when the current-prefix-arg is not null

--- basic-scripts/network-util.el
+++ basic-scripts/network-util.el
@@ -5,17 +5,30 @@
 (defun current-ip ()
   "."
   (interactive)
+  (let* ((shell-command-string "ifconfig $(route -n | grep ^0.0.0.0 | awk '{print $NF}') | grep 'inet ' | grep -v 127.0.0.1 | awk '{print $2'}")
+         (result (shell-command-to-string shell-command-string))
+         (result (substring result 0 (- (length result) 1))))
+    (when current-prefix-arg
+      (message "current-ip: %s" result))
+    result))
+
+(defun current-ip-2 ()
+  "."
+  (interactive)
   (let* ((ruby-command-string "print Socket.ip_address_list.find { |ai| ai.ipv4? && !ai.ipv4_loopback? }.ip_address")
          (shell-command-string (format "ruby -r socket -e \"%s\"" ruby-command-string))
          (result (shell-command-to-string shell-command-string)))
     (message result)))
 
-(defun show-public-ip ()
+(defun public-ip ()
   "."
   (interactive)
   (let* ((shell-command-string "curl -s cip.cc | grep IP | cut -f2 -d ':' | awk '{ gsub(/^[ \t]+|[ \t]+$/, \"\"); print }'")
-         (result (shell-command-to-string shell-command-string)))
-    (message result)))
+         (result (shell-command-to-string shell-command-string))
+         (result (substring result 0 (- (length result) 1))))
+    (when current-prefix-arg
+      (message "public-ip: %s" result))
+    result))
 
 (defun switch-proxy (enable)
   "ENABLE's value is t or nil."
