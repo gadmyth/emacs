38faa928ccb7f5bdc45602142b180ee2a5d2ddf3
gadmyth <gadmyth@gmail.com>
2021-09-17 12:58:56 +0800

    buffers: switch-scratch-buffers add *Message*, *Help* buffer to list

--- basic-scripts/buffers.el
+++ basic-scripts/buffers.el
@@ -22,16 +22,22 @@
     (funcall (and initial-major-mode))
     (setq buffer-offer-save t)))
 
+(defvar *quick-buffer-prefix-list* '("*scratch*" "*Messages*" "*Help*"))
+
 (defun switch-scratch-buffers (&rest _)
   "NAME: ."
   (interactive)
-  (let* ((scratch-buffers (seq-filter (lambda (buffer) (string-prefix-p "*scratch*" (buffer-name buffer))) (buffer-list)))
-         (name (completing-read "Switch to buffer: " (sort (mapcar #'buffer-name scratch-buffers) 'string<) nil t nil nil nil))
+  (let* ((quick-buffers (seq-filter (lambda (buffer)
+                                      (let ((matched-list (mapcar (lambda (name) (string-prefix-p name (buffer-name buffer))) *quick-buffer-prefix-list*)))
+                                        (eval `(or ,@matched-list))))
+                                    (buffer-list)))
+         (quick-buffers (sort (mapcar #'buffer-name quick-buffers) 'string<))
+         (quick-buffers (cons "*scratch*" (delete "*scratch*" quick-buffers)))
+         (name (completing-read "Switch to buffer: " quick-buffers nil t nil nil nil))
          (buffer (get-buffer name)))
     (if (buffer-live-p buffer)
         (progn
           (switch-to-buffer buffer)
-          ;(funcall (and initial-major-mode))
           (setq buffer-offer-save t))
       (message (format "The buffer %s doesn't exist!" name)))))
 
