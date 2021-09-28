3c3137b3f6c56f964209b8e416c2c12b36ceb9a2
gadmyth <gadmyth@gmail.com>
2021-09-28 20:40:00 +0800

    erc+: fix error when load *erc-aggregate-buffer*

--- basic-scripts/erc+.el
+++ basic-scripts/erc+.el
@@ -3,8 +3,8 @@
 ;; Copyright (C) 2021 gadmyth
 
 ;; Author: erc+.el <gadmyth@gmail.com>
-;; Version: 1.0.022
-;; Package-Version: 20210924.002
+;; Version: 1.0.023
+;; Package-Version: 20210928.001
 ;; Package-Requires: erc, s, text-mode, system-util
 ;; Keywords: erc+.el
 ;; Homepage: https://www.github.com/gadmyth/emacs
@@ -183,8 +183,8 @@ With PARSED message and PROC."
     (when (not (buffer-live-p *erc-aggregate-buffer*))
       ;; create buffer if not exists
       (setq *erc-aggregate-buffer*
-            (generate-new-buffer "*erc-aggregate-buffer*")
-            (load-erc-forbidden-targets))
+            (generate-new-buffer "*erc-aggregate-buffer*"))
+      (load-erc-forbidden-targets))
       ;; set the major mode
       (with-current-buffer *erc-aggregate-buffer*
         (erc-aggregate-mode)))
