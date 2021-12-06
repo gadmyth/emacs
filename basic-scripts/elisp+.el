5e41033a9d421cef80495080fe4e9090578d9909
gadmyth <gadmyth@gmail.com>
2021-12-06 13:49:48 +0800

    elisp+: add eval-region-with-context method

--- basic-scripts/elisp+.el
+++ basic-scripts/elisp+.el
@@ -21,8 +21,23 @@
     (error (message "Invalid expression")
            (insert (current-kill 0)))))
 
+(defun eval-region-with-context (beg end)
+  "."
+  (interactive "r")
+  (let* ((content (buffer-substring-no-properties beg end))
+         (content (format "(%s)" content))
+         (list (read content))
+         (msg ""))
+    (dolist (pair list)
+      (let* ((key (car pair))
+             (value-exp (cadr pair))
+             (value (eval value-exp)))
+        (setq msg (concat msg (format "%S: %S" key value) "\n"))))
+    (message msg)))
+
 (global-set-key (kbd "C-c e r") 'eval-and-replace)
 (global-set-key (kbd "C-c e d") 'eval-and-delete)
+(global-set-key (kbd "C-c e c") 'eval-region-with-context)
 
 (provide 'elisp+)
 ;;; elisp+.el ends here
