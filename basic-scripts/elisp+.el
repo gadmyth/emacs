09da99c521e1c88621669085b1e386fa0c1d5b13
gadmyth <gadmyth@gmail.com>
2021-12-06 23:27:44 +0800

    elisp+: fix bug of eval-region-with-context

--- basic-scripts/elisp+.el
+++ basic-scripts/elisp+.el
@@ -22,18 +22,28 @@
            (insert (current-kill 0)))))
 
 (defun eval-region-with-context (beg end)
-  "."
+  "Eval the region of the let pairs from BEG to END."
   (interactive "r")
   (let* ((content (buffer-substring-no-properties beg end))
          (content (format "(%s)" content))
          (list (read content))
-         (msg ""))
-    (dolist (pair list)
-      (let* ((key (car pair))
-             (value-exp (cadr pair))
-             (value (eval value-exp)))
-        (setq msg (concat msg (format "%S: %S" key value) "\n"))))
-    (message msg)))
+         (msg "The eval result:"))
+    (eval (generate-let-context
+           list
+           (lambda (content)
+             (setq msg (format "%s\n%s" msg content)))))
+    (message "%s" msg)))
+
+(defun generate-let-context (list callback)
+  "Generate let context with contexts of LIST, call CALLBACK every level of let body."
+  (if (= (length list) 0)
+      nil
+    (let* ((pair (car list))
+           (key (car pair))
+           (value-exp (cadr pair)))
+      `(let (,pair)
+         (funcall ,callback (format "%s: %s" ',key ,value-exp))
+         ,(generate-let-context (cdr list) callback)))))
 
 (global-set-key (kbd "C-c e r") 'eval-and-replace)
 (global-set-key (kbd "C-c e d") 'eval-and-delete)
