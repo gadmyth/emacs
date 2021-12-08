ec2b12add397b04d10a2b7dd5018bbb58e05d0ea
gadmyth <gadmyth@gmail.com>
2021-12-08 11:09:55 +0800

    add rest-util.el, add util functions

---
+++ basic-scripts/rest-util.el
@@ -0,0 +1,23 @@
+;;; package --- rest-util.el
+;;; Commentary:
+;;; Code:
+
+(require 's)
+(require 'url-cookie)
+
+(defun store-browser-cookies (domain &rest cookie-keys)
+  "Get the browser cookies of DOMAIN whoice key's is COOKIE-KEYS."
+  (let* ((param (s-join ":" cookie-keys))
+         (scriptlets-dir (expand-file-name "scriptlets" +emacs-context-directory+))
+         (command (format "python %s %s %s" (expand-file-name "get-browser-cookies.py" scriptlets-dir) domain param))
+         (result (shell-command-to-string command)))
+    (dolist (cookie-line (s-split "\n" result))
+      (when (> (length cookie-line) 0)
+        (let* ((cookie-pair (s-split " " cookie-line))
+               (cookie-key (car cookie-pair))
+               (cookie-value (cadr cookie-pair)))
+          (message "cookie:\nkey: %s\nvalue: %s\n\n" cookie-key cookie-value)
+          (url-cookie-store cookie-key cookie-value nil domain "/" t))))))
+
+(provide 'rest-util)
+;;; rest-util.el ends here
