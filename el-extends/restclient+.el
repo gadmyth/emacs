7e5695e46cde41a7e11a4e9a2aa1a183e11610d8
gadmyth <gadmyth@gmail.com>
2021-12-08 09:44:15 +0800

    restclient+: fix bug of can't show non json result

--- el-extends/restclient+.el
+++ el-extends/restclient+.el
@@ -29,25 +29,42 @@
  (defvar rc--current-token nil)
  (defvar rc--verify-code nil)
  (defvar rc--uap-token nil)
+
+ (defun restclient-guess-mode ()
+   "Copied from restclient.el."
+   (save-excursion
+     (let ((start (point))
+           (guessed-mode)
+           (end-of-headers))
+       (while (and (not (looking-at restclient-empty-line-regexp))
+                   (eq (progn
+                         (when (looking-at restclient-content-type-regexp)
+                           (setq guessed-mode
+                                 (cdr (assoc-string
+                                       (concat (match-string-no-properties 1) "/" (match-string-no-properties 2))
+                                       restclient-content-type-modes t))))
+                         (forward-line)) 0)))
+       guessed-mode)))
+
+ (defun restclient-parse-token ()
+   "."
+   (when (eq (restclient-guess-mode) 'js-mode)
+     (let* ((result-object (json-read))
+            (access-token (restclient-parse-from-alist result-object 'accessToken))
+            (verify-code (restclient-parse-from-alist result-object 'verifycode))
+            (token (restclient-parse-from-alist result-object 'token)))
+       (when access-token
+         (setq rc--current-token access-token)
+         (message "access-token: %S" access-token))
+       (when verify-code
+         (setq rc--verify-code verify-code)
+         (message "verify-code: %S" verify-code))
+       (when token
+         (setq rc--uap-token token)
+         (message "token: %S" token)))))
  
- (setq restclient-response-loaded-hook
-       (lambda ()
-         (message "restclient-response-received-hook")
-         (let* ((result-object (json-read))
-                (access-token (restclient-parse-from-alist result-object 'accessToken))
-                (verify-code (restclient-parse-from-alist result-object 'verifycode))
-                (token (restclient-parse-from-alist result-object 'token)))
-           (when access-token
-             (setq rc--current-token access-token)
-             (message "access-token: %S" access-token))
-           (when verify-code
-             (setq rc--verify-code verify-code)
-             (message "verify-code: %S" verify-code))
-           (when token
-             (setq rc--uap-token token)
-             (message "token: %S" token))))))
-
-
+ (setq restclient-response-loaded-hook 'restclient-parse-token)
+ )
 
 (provide 'restclient+)
 ;;; restclient+.el ends here
