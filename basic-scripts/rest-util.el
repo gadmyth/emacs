61df8bfa8ddf8ddae04648074c257f2ac1beb10c
gadmyth <gadmyth@gmail.com>
2021-12-08 14:18:38 +0800

    rest-util: refactor code, remove s.el's dependency

--- basic-scripts/rest-util.el
+++ basic-scripts/rest-util.el
@@ -2,22 +2,25 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 's)
 (require 'url-cookie)
 
-(defun store-browser-cookies (domain cookie-keys)
+(defun refresh-cookies (domain cookie-keys)
   "Get the browser cookies of DOMAIN whoice key's is COOKIE-KEYS."
-  (let* ((param (s-join ":" cookie-keys))
+  (url-cookie-delete-cookies domain)
+  (let* ((param (mapconcat 'identity cookie-keys ":"))
          (scriptlets-dir (expand-file-name "scriptlets" +emacs-context-directory+))
-         (command (format "python %s %s %s" (expand-file-name "get-browser-cookies.py" scriptlets-dir) domain param))
-         (result (shell-command-to-string command)))
-    (dolist (cookie-line (s-split "\n" result))
-      (when (> (length cookie-line) 0)
-        (let* ((cookie-pair (s-split " " cookie-line))
-               (cookie-key (car cookie-pair))
-               (cookie-value (cadr cookie-pair)))
-          (message "cookie:\nkey: %s\nvalue: %s\n\n" cookie-key cookie-value)
-          (url-cookie-store cookie-key cookie-value nil domain "/" t))))))
+         (python-command (executable-find "python3"))
+         (script-path (expand-file-name "get-browser-cookies.py" scriptlets-dir))
+         (command (format "%s %s %s %s" python-command script-path domain param))
+         (result (shell-command-to-string command))
+         (result (split-string result "\n"))
+         (cookie-list (seq-filter (lambda (s) (not (string-empty-p s))) result))
+         (cookie-pairs (seq-map (lambda (s) (split-string s)) cookie-list)))
+    (dolist (cookie-pair cookie-pairs)
+      (let* ((cookie-key (car cookie-pair))
+             (cookie-value (cadr cookie-pair)))
+        (message "cookie:\nkey: %s\nvalue: %s\n\n" cookie-key cookie-value)
+        (url-cookie-store cookie-key cookie-value nil domain "/" t)))))
 
 (provide 'rest-util)
 ;;; rest-util.el ends here
