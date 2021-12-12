3429a137935b29e998f9a59149300366fba559f6
gadmyth <gadmyth@gmail.com>
2021-12-12 17:33:16 +0800

    rest-util: update get-browser-cookies function

--- scriptlets/get-browser-cookies.py
+++ scriptlets/get-browser-cookies.py
@@ -7,10 +7,11 @@ def main():
     cookiejar = None
     domain = sys.argv[1]
     cookie_keys = sys.argv[2].split(":")
-    try:
-        cookiejar = browser_cookie3.chrome(domain_name=domain)
-    except Exception:
-        print("get cookie from Chrome failed", file=sys.stderr)
+    if not cookiejar:
+        try:
+            cookiejar = browser_cookie3.chrome(domain_name=domain)
+        except Exception:
+            print("get cookie from Chrome failed", file=sys.stderr)
 
     if not cookiejar:
         try:
@@ -19,14 +20,19 @@ def main():
             print("get cookie from Firefox failed", file=sys.stderr)
             return
 
+    for cookie in cookiejar:
+        print("browser cookie name:", cookie.name)
+        
     cookies = list(filter(lambda c: c.name in cookie_keys, cookiejar))
 
-    if len(cookies) < 4:
+    #print("cookies length: %d, cookie_keys length: %d" % (len(cookies), len(cookie_keys)))
+    
+    if len(cookies) < len(cookie_keys):
         print("get cookie failed, make sure you have Chrome or Firefox installed and login with one of them at least once.")
         return
 
     for c in cookies:
-        print(c.name, c.value)
+        print("cookie pair:%s:%s" % (c.name, c.value))
 
 
 if __name__ == "__main__":
