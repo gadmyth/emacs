2422fd8e02a6cca35756260f7e1539a9b911a6c9
gadmyth <gadmyth@gmail.com>
2021-12-13 23:15:59 +0800

    notifications: support repeatable notification

--- basic-scripts/notifications.el
+++ basic-scripts/notifications.el
@@ -3,9 +3,9 @@
 ;; Copyright (C) 2021 gadmyth
 
 ;; Author: notifications.el <gadmyth@gmail.com>
-;; Version: 1.0.6
-;; Package-Version: 20211205.002
-;; Package-Requires: async, dates, codec
+;; Version: 1.0.7
+;; Package-Version: 20211213.001
+;; Package-Requires: timer, dates, codec
 ;; Keywords: notification, notify
 ;; Homepage: https://www.github.com/gadmyth/emacs
 ;; URL: https://www.github.com/gadmyth/emacs/blob/master/basic-scripts/notifications.el
@@ -34,13 +34,14 @@
 ;;; Code:
 
 
-(require 'async)
+(require 'timer)
 (require 'dates)
 (require 'codec)
+(require 'uuid)
 
 (defvar *notifications* nil)
 
-(defvar *notifications-buffer* "*notification*")
+(defvar *notifications-buffer* "*notifications*")
 
 (defvar +notifications-file-name+ (expand-file-name "~/.emacs.notifications"))
 
@@ -48,20 +49,20 @@
   "."
   (interactive)
   (let* ((notifications (mapcar (lambda (notification)
-                                  (let* ((message (cdr (assq 'message notification)))
+                                  (let* ((message (alist-get 'message notification))
                                          (message (base64-decode-string-as-multibyte message))
-                                         (timestamp (cdr (assq 'timestamp notification)))
-                                         (timestr (timestamp-to-normal-string timestamp)))
-                                    (format "%s (%s): %s" timestr (notification-time-diff-description timestamp) message)))
+                                         (fire-time (alist-get 'fire-time notification))
+                                         (fire-time-str (timestamp-to-normal-string fire-time)))
+                                    (format "%s (%s): %s" fire-time-str (notification-time-diff-description fire-time) message)))
                                 *notifications*))
          (notifications (sort notifications #'string<))
          (current-timestr (current-time-normal-string)))
     (completing-read (format "The notifications (%s): " current-timestr) notifications)))
 
-(defun notification-time-diff-description (timestamp)
-  "Show the time diff description of TIMESTAMP and now."
+(defun notification-time-diff-description (fire-time)
+  "Show the time diff description of FIRE-TIME and now."
   (let* ((now (current-timestamp))
-         (diff (- timestamp now))
+         (diff (- fire-time now))
          (seconds diff)
          (minutes (/ diff 60))
          (hours (/ diff 3600))
@@ -93,26 +94,40 @@
       (setq desc (s-concat desc (format "%ds" seconds))))
     desc))
 
-(defun start-notify (message arg2)
-  "Show MESSAGE as notification after some minutes or at some certain time of ARG2."
+(defun start-notify (message arg2 arg3)
+  "Show MESSAGE as notification after some minutes or at some certain time of ARG2, ARG3."
   (interactive
    (list
     (read-string "notification to send: " nil nil nil)
     (if (null current-prefix-arg)
         (read-number "after minitues: " 0)
-      (read-string "at the time: " (current-time-normal-string)))))
-  (let* ((current-time-stamp (current-timestamp))
-         (minutes (if (null current-prefix-arg)
-                      arg2 (/ (- (string-to-timestamp arg2)
-                                 (current-timestamp))
-                              60.0)))
-         (timestamp (+ current-time-stamp (* 60.0 minutes))))
-    (when (> minutes 0)
+      (read-string "at the time: " (current-time-normal-string)))
+    (read-string "repeat duration: " "0m")))
+  (let* ((now (current-timestamp))
+         (seconds (if (null current-prefix-arg)
+                      (* 60 arg2)
+                    (- (string-to-timestamp arg2) now))))
+    (when (> seconds 0)
       (let* ((message (base64-encode-string-of-multibyte message))
-             (notification `((timestamp . ,timestamp) (message . ,message)))
-             (notify-string (base64-encode-string-of-multibyte (format "%s" notification))))
-        (add-to-list '*notifications* notification)
-        (do-start-notify notify-string minutes)))))
+             (fire-time (+ now seconds))
+             (index (- (length arg3) 1))
+             (repeat-duration-number (string-to-number (substring arg3 0 index)))
+             (repeat-duration-unit (substring arg3 index))
+             (repeat-duration (pcase repeat-duration-unit
+                                ("m" (* 60 repeat-duration-number))
+                                ("h" (* 60 60 repeat-duration-number))
+                                ("d" (* 24 60 60 repeat-duration-number))
+                                ("M" (* 30 24 60 60 repeat-duration-number))
+                                (_ repeat-duration-number)))
+             (id (replace-regexp-in-string "-" "" (uuid-string)))
+             (notification `((id . ,id)
+                             (fire-time . ,fire-time)
+                             (message . ,message)
+                             (repeat-duration . ,repeat-duration))))
+        (message "a")
+        (update-notification notification)
+        (message "%S" notification)
+        (do-schedule-notification id seconds)))))
 
 (defun load-notifications ()
   "."
@@ -122,33 +137,83 @@
       (goto-char (point-min))
       (let ((content (read (current-buffer))))
         (when (> (length content) 0)
-          (setq *notifications* content)
           (remove-expired-notifications)
           (let ((now (current-timestamp)))
-            (dolist (notification *notifications*)
-              (let* ((timestamp (cdr (assq 'timestamp notification)))
-                     (minutes (/ (- timestamp now) 60.0))
-                     (notify-string (base64-encode-string-of-multibyte (format "%s" notification))))
-                (message "start notify: %S" notification)
-                (do-start-notify notify-string minutes))))))))
+            (dolist (notification content)
+              (load-notification notification)))))))
   t)
+
+(defun load-notification (notification)
+  "Load the NOTIFICATION if not exist in *notifications* list."
+  (let ((id (alist-get 'id notification))
+        (noti (get-notification id)))
+    (when (not noti)
+      (setf (get-notification id) notification)
+      (schedule-notification notification))))
+
+(defun schedule-notification (id)
+  "Schedule the notification of ID."
+  (let* ((notification (get-notification id))
+         (scheduled (alist-get 'scheduled notification))
+         (now (current-timestamp))
+         (fire-time (alist-get 'fire-time notification)))
+    (when (and (not scheduled)
+               (> fire-time now))
+      (let ((seconds (- fire-time now)))
+        (message "now schedule the notification: %S" notification)
+        (do-schedule-notification id seconds)))))
 
 (defun remove-expired-notifications ()
   "."
   (let ((now (current-timestamp)))
-    (setq *notifications*
-          (remove-if
-           (lambda (notification)
-             (> now (cdr (assq 'timestamp notification))))
-           *notifications*))))
+    (dolist (noti *notifications*)
+      (let* ((id (car noti))
+             (notification (cdr noti))
+             (fire-time (alist-get 'fire-time notification))
+             (repeat-duration (alist-get 'repeat-duration notification)))
+        (when (and (> now fire-time)
+                   (= repeat-duration 0.0))
+          (setf (get-notification id) nil))))))
 
-(defun remove-the-notifications (timestamp)
-  "Remove notification who's timestamp is TIMESTAMP."
-  (setq *notifications*
-        (remove-if
-         (lambda (notification)
-           (= timestamp (cdr (assq 'timestamp notification))))
-         *notifications*)))
+(defun remove-fired-notifications ()
+  "Remove FIRED notifications."
+  (dolist (noti *notifications*)
+    (let* ((id (car noti))
+           (notification (cdr noti))
+           (fired (alist-get 'fired notification))
+           (repeat-duration (alist-get 'repeat-duration notification)))
+      (when (and fired
+                 (= repeat-duration 0.0))
+        (setf (get-notification id) nil)))))
+
+(defun refresh-repeatable-notifications ()
+  "."
+  (let ((now (current-timestamp)))
+    (dolist (notification *notifications*)
+      (refresh-repeatable-notification notification))))
+
+(defun refresh-repeatable-notification (id)
+  "Refresh the fire time of the fired repeatable notification of ID."
+  (let* ((notification (get-notification id))
+         (fire-time (alist-get 'fire-time notification))
+         (repeat-duration (alist-get 'repeat-duration notification))
+         (fired (alist-get 'fired notification))
+         (now (current-timestamp)))
+    (message "AA: fire-time: %S, repeat-duration: %S, fired: %S" fire-time repeat-duration fired)
+    (when (and fired
+               (>= now fire-time)
+               repeat-duration
+               (> repeat-duration 0.0))
+      (let* ((multiple (floor (/ (- now fire-time) repeat-duration)))
+             (new-fire-time (+ fire-time (* (+ multiple 1) repeat-duration))))
+        ;; refresh the new fire time
+        (set-notify-property notification 'fire-time new-fire-time)
+        ;; reset the fired mark
+        (set-notify-property notification 'fired nil)
+        ;; update notification back to list
+        (update-notification notification)
+        ;; re-schedule the repeatable notification
+        (schedule-notification id)))))
 
 (defun save-notifications ()
   "."
@@ -157,27 +222,44 @@
     (with-temp-file +notifications-file-name+
       (insert content))))
 
-(defun do-start-notify (notification minutes)
-  "Show message of NOTIFICATION after some MINUTES."
-  (async-start
+(defun do-schedule-notification (id seconds)
+  "Show notification message which id is ID after some SECONDS."
+  (run-with-timer
+   seconds
+   nil
    `(lambda ()
-      (let ((seconds (* 60 ,minutes)))
-        (sleep-for seconds)
-        ,notification))
-   `(lambda (arg)
-      (let* ((notification (base64-decode-string-as-multibyte arg))
-             (notification (read notification)))
+      (let ((notification (get-notification ,id)))
         (message "received notification: %S" notification)
-        (let* ((message (format "%s" (cdr (assq 'message notification))))
+        (let* ((message (format "%s" (alist-get 'message notification)))
                (message (base64-decode-string-as-multibyte message))
-               (timestamp (cdr (assq 'timestamp notification))))
-          (with-current-buffer (get-buffer-create *notifications-buffer*)
+               (id (alist-get 'id notification))
+               (fire-time (alist-get 'fire-time notification))
+               (buffer (get-buffer-create *notifications-buffer*)))
+          (with-current-buffer buffer
             (read-only-mode 0)
             (goto-char (point-max))
-            (insert (format-time-string "%Y-%m-%d %H:%M:%S" (current-time)) "\n" message "\n\n")
+            (insert (format-time-string "%Y-%m-%d %H:%M:%S" fire-time) "\n" message "\n\n")
             (read-only-mode t))
-          (display-buffer "*notification*")
-          (remove-the-notifications timestamp))))))
+          (display-buffer buffer)
+          ;; mark as fired
+          (set-notify-property notification 'fired t)
+          (update-notification notification)
+          ;; re-schedule repeatable notification
+          (refresh-repeatable-notification id)
+          (remove-fired-notifications))))))
+
+(defun update-notification (notification)
+  "NOTIFICATION."
+  (when-let ((id (alist-get 'id notification)))
+    (setf (get-notification id) notification)))
+
+(defmacro set-notify-property (notification key value)
+  "Update NOTIFICATION's VALUE of KEY."
+  `(setf (alist-get ,key ,notification) ,value))
+
+(defmacro get-notification (id)
+  "ID."
+  `(alist-get ,id *notifications* nil t 'equal))
 
 (global-set-key (kbd "C-x s") 'start-notify)
 
@@ -187,7 +269,8 @@
               (when (load-notifications)
                 ;; add the save-notifications to 'kill-emacs-hook after load file success,
                 ;;or it will save empty content to file dangerously.
-                (add-hook 'kill-<emacs-hook 'save-notifications))))
+                (add-hook 'kill-emacs-hook 'save-notifications))))
+
 
 (provide 'notifications)
 ;;; notifications.el ends here
