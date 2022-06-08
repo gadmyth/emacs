816cf46faa085c88fc6aa3cbec64c2c7a45c4b9f
gadmyth <gadmyth@gmail.com>
2022-06-08 22:52:15 +0800

    org-mode+: refactor code; support open org link with external tools

--- basic-scripts/org-mode+.el
+++ basic-scripts/org-mode+.el
@@ -1,7 +1,41 @@
-;;; package --- org-mode+.el
+;;; org-mode+.el --- Package.  -*- lexical-binding: nil; -*-
+
+;; Copyright (C) 2022 gadmyth
+
+;; Author: org-mode+.el <gadmyth@gmail.com>
+;; Version: 1.0.0
+;; Package-Version: 20220608.001
+;; Package-Requires: org, dash, browse-url+
+;; Keywords: org-mode
+;; Homepage: https://www.github.com/gadmyth/emacs
+;; URL: https://www.github.com/gadmyth/emacs/blob/master/basic-scripts/org-mode+.el
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Source code
+;;
+;; org-mode+'s code can be found here:
+;;   https://www.github.com/gadmyth/emacs/blob/master/basic-scripts/org-mode+.el
+
 ;;; Commentary:
 ;;; Code:
 
+(require 'dired)
+(require 'dash)
+(require 'browse-url+)
 
 (defvar *org-cap-temp*)
 (defvar *temp-org-capture-buffer* nil)
@@ -26,11 +60,11 @@
     (forward-line -1)
     (let* ((line (replace-regexp-in-string "-" "" (org-id-uuid)))
            (filename (replace-regexp-in-string (getenv "HOME") "${HOME}" (buffer-file-name)))
-           (formatted-line (format "[[file:%s::%s][%s]]" filename line description)))
+           (link (format "file:%s::%s" filename line)))
       (insert (format "MARK: %s" line))
       (indent-for-tab-command)
       (comment-line 1)
-      (setq *org-cap-temp* formatted-line)
+      (setq *org-cap-temp* `((link . ,link) (description . ,description)))
       (when show-temp-capture-buffer-p
         (display-temp-capture-buffer)))))
 
@@ -38,8 +72,8 @@
   "DESCRIPTION: ."
   (interactive "sSet the line description here: ")
   (let* ((filename (replace-regexp-in-string (getenv "HOME") "${HOME}" (buffer-file-name)))
-         (formatted-line (format "[[file:%s][%s]]" filename description)))
-    (setq *org-cap-temp* formatted-line)
+         (link (format "file:%s" filename)))
+    (setq *org-cap-temp* `((link . ,link) (description . ,description)))
     (when show-temp-capture-buffer-p
       (display-temp-capture-buffer))))
 
@@ -53,8 +87,8 @@
   (let* ((line (match-string-no-properties 1))
          (encoded-line (url-encode-url line))
          (filename (replace-regexp-in-string (getenv "HOME") "${HOME}" (buffer-file-name)))
-         (formatted-line (format "[[file:%s::%s][%s]]" filename line description)))
-    (setq *org-cap-temp* formatted-line)
+         (link (format "file:%s::%s" filename line)))
+    (setq *org-cap-temp* `((link . ,link) (description . ,description)))
     (when show-temp-capture-buffer-p
       (display-temp-capture-buffer))))
 
@@ -66,8 +100,9 @@
 (defun org-capture-mark ()
   "."
   (interactive)
-  (let* ((action (completing-read "Please choose the action: " *org-capture-actions* nil t nil nil nil))
-         (func (alist-get action *org-capture-actions* nil nil #'string=)))
+  (let* ((action (completing-read "Please choose the action: "
+                                  *org-capture-actions* nil t nil nil nil))
+         (func (alist-get action *org-capture-actions* nil nil #'string-equal)))
     (message "%S" func)
     (when func
       (call-interactively func))))
@@ -75,17 +110,19 @@
 (defun org-capture-insert-temp ()
   "."
   (interactive)
-  (if (not (null *org-cap-temp*))
-      (progn
-        (insert *org-cap-temp*)
-        (setq *org-cap-temp* nil))))
+  (when (> (length *org-cap-temp*) 0)
+    (let* ((link (alist-get "link" *org-cap-temp* nil nil #'string-equal))
+           (description (alist-get "description" *org-cap-temp* nil nil #'string-equal))
+           (content (format "[[%s][%s]]" link description)))
+      (insert content)
+      (setq *org-cap-temp* nil))))
 
 (defun org-capture-dired-file (description)
   "DESCRIPTION: ."
   (interactive "sSet the dir description here: ")
   (let* ((file (dired-get-file-for-visit))
-        (formatted-line (format "[[file:%s][%s]]" file description)))
-    (setq *org-cap-temp* formatted-line)
+         (link (format "file:%s" file)))
+    (setq *org-cap-temp* `((link . ,link) (description . ,description)))
     ;(visit-work-file)
     ))
 
@@ -98,8 +135,11 @@
         (let* ((org-link-file (match-string-no-properties 1))
                (org-link-dir (if (file-directory-p org-link-file)
                                  org-link-file
-                               (file-name-directory org-link-file))))
-          (cons org-link-file org-link-dir))
+                               (file-name-directory org-link-file)))
+               (file (if (file-name-absolute-p org-link-file) org-link-file
+                       (expand-file-name org-link-file default-directory)))
+               (dir org-link-dir))
+          (cons file dir))
       (cons nil nil))))
 
 (defun org-make-element-link ()
@@ -134,6 +174,7 @@
   (let* ((parse-result (org-parse-file-link))
          (file (car parse-result))
          (dir (cdr parse-result)))
+    (message "file: %s, dir: %s" file dir)
     (when (and (not (null file))
                (not (null dir)))
       (dired-other-window dir)
@@ -141,7 +182,7 @@
       (message "Selected file is: %s" file))))
 
 (defun org-show-link ()
-  "."
+   "."
   (interactive)
   (let* ((parse-result (org-parse-file-link))
          (file (car parse-result))
@@ -149,16 +190,64 @@
     (if (and (not (null file)) (not (null dir)))
       (message "The file of link is: %s" file))))
 
+(defun org-parse-link ()
+  "."
+  (save-excursion
+    (re-search-backward "^" nil t)
+    (if (re-search-forward "^.*?\\(\\(https?\\|file\\):[^]]*\\).*$" nil t 1)
+        (let* ((org-link (match-string-no-properties 1)))
+          org-link))))
+
 (defun org-copy-link ()
   "."
   (interactive)
-  (save-excursion
-    (re-search-backward "^" nil t)
-    (if (re-search-forward "^.*?\\(https?:[^]]*\\).*$" nil t 1)
-        (let* ((org-link (match-string-no-properties 1)))
-          (kill-new org-link)
-          (message org-link)
-          org-link))))
+  (when-let ((org-link (org-parse-link)))
+    (kill-new org-link)
+    (message org-link)))
+
+(defvar *org-link-program-alist*
+  `(("\\.\\(jpe?g\\|png\\)\\'"
+     ,(cond ((eq window-system 'x)
+             '("ristretto" "gImageReader"))
+            (t nil)))
+    ("\\.xlsx\\'"
+     ,(cond ((eq window-system 'x)
+             '("gnumeric"))
+            (t nil)))
+    ("\\.htm?l\\'"
+     ,(cond ((eq window-system 'x)
+             '("google-chrome" "firefox"))
+            (t nil)))
+    ("\\https?://.*\\'"
+     ,(cond ((eq window-system 'x)
+             '("google-chrome" "firefox"))
+            (t nil)))))
+
+(defun org-open-link-external ()
+  "."
+  (interactive)
+  (when-let ((org-link (org-parse-link)))
+    (cond
+     ((string-prefix-p "http" org-link)
+      (browse-url-select-function org-link))
+     ((string-prefix-p "file:" org-link)
+      (let* ((path (substring org-link (length "file:")))
+             (path (or (and (file-name-absolute-p path) path)
+                       (format "%s/%s" default-directory path)))
+             (programs (org-filter-link-programs path))
+             (program (completing-read "Select the command: " programs nil t)))
+        (message "file path is %s" path)
+        (when program
+          (start-process program nil program path)))))))
+
+(defun org-filter-link-programs (param)
+  "Filter the programs of PARAM with match the alist's REGEXP."
+  (-flatten
+   (mapcar #'cdr
+           (seq-filter
+            (lambda (elem)
+              (string-match-p (car elem) param))
+            *org-link-program-alist*))))
 
 (global-set-key (kbd "<f7>") 'org-capture-mark)
 (global-set-key (kbd "<M-f7>") 'org-capture-insert-temp)
