8f356b205d1bdf5cf634563894408b44865f72bb
gadmyth <gadmyth@gmail.com>
2022-06-14 10:47:41 +0800

    browse-url+: support xwwp and xwidget to browse url

--- basic-scripts/browse-url+.el
+++ basic-scripts/browse-url+.el
@@ -3,8 +3,8 @@
 ;; Copyright (C) 2022 Gadmyth
 
 ;; Author: browse-url+.el <gadmyth@gmail.com>
-;; Version: 1.0.1
-;; Package-Version: 20220612.001
+;; Version: 1.0.2
+;; Package-Version: 20220614.001
 ;; Package-Requires: w3m
 ;; Keywords: browse url
 ;; Homepage: https://www.github.com/gadmyth/emacs
@@ -38,6 +38,11 @@
     (eww-browse-url . browse-url)
     ,(if (featurep 'w3m-load)
          `(w3m-browse-url . browse-url))
+    ,(cond
+      ((featurep 'xwwp)
+       `(xwwp . browse-url))
+      ((featurep 'xwidget)
+       `(xwidget-webkit-browse-url . browse-url)))
     (browse-url-firefox . browse-url)
     (browse-url-chrome . browse-url)
     (show-link-url . show-link-url)))
