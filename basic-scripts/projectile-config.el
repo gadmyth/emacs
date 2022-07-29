26b9dab429927c2389de682bece72d4f47e04932
gadmyth <gadmyth@gmail.com>
2022-07-29 18:08:01 +0800

    projectile-config: don't check package installed or not

--- basic-scripts/projectile-config.el
+++ basic-scripts/projectile-config.el
@@ -3,12 +3,12 @@
 ;;; Code:
 
 
-(when (package-installed-p 'projectile)
-  (require 'projectile)
-  (projectile-global-mode)
-  (setq projectile-indexing-method 'native)
-  (setq projectile-enable-caching t)
-  (setq projectile-file-exists-remote-cache-expire nil))
+(require 'projectile)
+
+(projectile-global-mode)
+(setq projectile-indexing-method 'native)
+(setq projectile-enable-caching t)
+(setq projectile-file-exists-remote-cache-expire nil)
 
 (provide 'projectile-config)
 ;;; projectile-config.el ends here
