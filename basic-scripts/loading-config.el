304513bf9b7e02e69c0b39b8f7241d200172dfae
gadmyth <gadmyth@gmail.com>
2022-07-29 17:32:38 +0800

    loading-config: use internal with-timeout instead of async dependency

--- basic-scripts/loading-config.el
+++ basic-scripts/loading-config.el
@@ -2,20 +2,19 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 'async-config)
 
-(setq gc-cons-threshold (* 50 1000 1000))
+(setq gc-cons-threshold (* 1000 1000 1000))
 (setq garbage-collection-messages t)
 
 (add-hook
  'emacs-startup-hook
  (lambda ()
-   (call-after
-    1
-    ;; This could use (emacs-init-time)
-    (message "**** Emacs startup end in %s with %d garbage collections *****"
-             (emacs-init-time)
-             gcs-done))))
+   (with-timeout (1 nil)
+     ;; This could use (emacs-init-time)
+     (message "**** Emacs startup end in %s with %d garbage collections *****"
+              (emacs-init-time)
+              gcs-done))))
+
 
 (provide 'loading-config)
 ;;; loading-config.el ends here
