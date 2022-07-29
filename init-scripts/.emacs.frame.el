9c41c15f46bb2ae6afadfb3321864d4eca3c3adf
gadmyth <gadmyth@gmail.com>
2022-07-29 18:09:17 +0800

    .emacs.frame.el: don't download +required-packages+; refactor code, fix loading error when package can't download

--- init-scripts/.emacs.frame.el
+++ init-scripts/.emacs.frame.el
@@ -14,48 +14,57 @@
  (load-pre-script-files))
 
 ;; insall packages
+;; should download packages
 (require-package
  'packages
- (package-initialize)
- (install-packages-if-needed +required-packages+))
+ (package-initialize))
 
 ;; load configs
 (require 'loading-config)
 (require 'basic-config)
 (require 'proced-config)
 (require 'servers)
-(require 'frames)
+(require 'utility)
+(require 'dates)
+(require 'elisp+)
+(require-package 'expand-region (global-set-key (kbd "C-=") 'er/expand-region))
+(require-package 'smartparens :dependencies '(dash) (smartparens-global-mode))
+(require 'scratch+)
+(require 'list-scratch)
 (require 'windows)
 (require 'buffers)
 (require 'mode-bars)
 (require 'fonts)
 (require 'shells)
-(require 'irc-config)
-(require 'telega-config)
 (require 'tab-config)
 (require 'clipboard)
+(require 'org-mode+)
 ;(require 'codings)
 (require 'codec)
 (require 'abbrev-config)
 (require 'uniquify-config)
-(require 'version-controll)
-(require 'swiper-config)
-(require 'org-config)
-(require 'gkroam-config)
 (require 'ido-config)
 (require 'eww-config)
-(require 'slime-config)
-(require 'projectile-config)
 (require 'scales)
-(require 'files-config)
-;(require 'evil-config)
-(require 'f-zone-key-bindings)
 (require 'calendar-config)
 (require 'dired-config)
+(require 'vc-config)
+;; depend on diff-hl
+(require 'version-controll)
+(require-package 'swiper-config :dependencies '(swiper counsel))
+(require 'org-config)
+(require 'gkroam-config)
+(require 'slime-config)
+(require-package 'projectile-config :dependencies '(projectile))
+(require 'files-config)
+;(require 'evil-config)
+;; depend on eyebrowse
+(require 'f-zone-key-bindings)
+;; depend on ac-emoji, swift-mode
 (require 'emoji-config)
 ;; download package
 (require 'web-config)
-(require 'smart-compile-config)
+(require-package 'smart-compile-config :dependencies '(smart-compile))
 (require 'python-config)
 (require 'lsp-config)
 ;; package + post config, in a seperate repo?? not in emacs.git, but should keep in cvs
@@ -63,40 +72,38 @@
 (require 'nov-config)
 ;; self config, require safely
 (require 'pdf-config)
-(require 'vc-config)
 (require-package 'redis-config :dependencies '(eredis))
 (require-package 'annotate-config :dependencies '(annotate))
-(require-package 'yas-config (yas-global-mode))
+(require-package 'yas-config :dependencies '(yasnippet dash ivy) (yas-global-mode))
 (require 'anythings)
 (require 'exwm-conf)
+;; depend on eyebrowse+, async
+(require-package 'frames :dependencies '(async))
+;; depend on erc+
+(require 'irc-config)
+;; depend on telega
+(require 'telega-config)
 
 ;; -------
 ;; network-util, rest-util, minibuffer+
 ;; -------
-(require 'q)
-(require 'utility)
-(require 'dates)
-(require 'elisp+)
-(require 'el-server)
-(require 'el-server-extend)
-(require 'org-mode+)
-(require 'goto-last-point+)
-(require 'scratch+)
-(require 'list-scratch)
+;; require el-node
+(require-package 'el-server :dependencies '(elnode))
+(require-package 'el-server-extend :dependencies '(elnode))
+;; depend on goto-last-point
+(require-package 'goto-last-point+ :dependencies '(goto-last-point))
 (require-package 'notifications :dependencies '(uuid))
 (require-package 'customized-dir (customized-dir-init))
 (require-package 'stopwatch :dependencies '(switch-buffer-functions) (stopwatch-mode 1))
-(require-package 'eyebrowse+ (eyebrowse-plus-mode 1))
-(require-package 'eyebrowse-xmonad (eyebrowse-xmonad-mode 1))
+(require-package 'eyebrowse+ :dependencies '(eyebrowse) (eyebrowse-plus-mode 1))
+(require-package 'eyebrowse-xmonad :dependencies '(eyebrowse s) (eyebrowse-xmonad-mode 1))
 (require-package 'wcy-desktop (wcy-desktop-init))
 (require-package 'password-generator+ :dependencies '(password-generator))
 
 
 ;; the following should move to el-post-scripts
 ;; package + post config
-(require 'sudo-edit)
-(require-package 'smartparens (smartparens-global-mode))
-(require-package 'expand-region (global-set-key (kbd "C-=") 'er/expand-region))
+(require-package 'sudo-edit)
 (require-package 'alpha (transparency-set-value *default-trans-value*))
 (require-package 'smex (global-set-key (kbd "M-x") 'smex))
 (require-package 'auto-complete (global-auto-complete-mode))
