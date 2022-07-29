b8f2d00f1cbc947bb36b72a522baa9ee069e44e5
gadmyth <gadmyth@gmail.com>
2022-07-29 17:29:30 +0800

    f-zone-key-bindings: fix error when eyebrowse is not installed

--- basic-scripts/f-zone-key-bindings.el
+++ basic-scripts/f-zone-key-bindings.el
@@ -3,7 +3,6 @@
 ;;; Code:
 
 
-(require 'eyebrowse+)
 (require 'customized-dir)
 (require 'minibuffer+)
 (require 'list-scratch)
@@ -12,25 +11,26 @@
 (global-set-key (kbd "<f1>") (toggle-minibuffer 'switch-scratch-buffers))
 (global-set-key (kbd "<M-f1>") 'new-buffer)
 
-;; eyebrowse buffer config
-(global-set-key (kbd "<f2>") (toggle-minibuffer 'eyebrowse-list-bookmarks))
-(global-set-key (kbd "<M-f2>") (toggle-minibuffer 'eyebrowse-modify-bookmark))
-(global-set-key (kbd "<C-f2>") 'eyebrowse-switch-bookmarks)
+;; eyebrowse config
+(require-package
+ 'eyebrowse+
+ :dependencies '(eyebrowse s dash)
+ (global-set-key (kbd "<f2>") (toggle-minibuffer 'eyebrowse-list-bookmarks))
+ (global-set-key (kbd "<M-f2>") (toggle-minibuffer 'eyebrowse-modify-bookmark))
+ (global-set-key (kbd "<C-f2>") 'eyebrowse-switch-bookmarks)
+ (global-set-key (kbd "<f5>") (toggle-minibuffer 'eyebrowse-list-configs))
+ (global-set-key (kbd "<M-f5>") (toggle-minibuffer 'eyebrowse-modify-config))
+ (global-set-key (kbd "<C-f5>") 'eyebrowse-last-window-config)
 
-;; eyebrowse buffer config
-(global-set-key (kbd "<f3>") (toggle-minibuffer 'eyebrowse-switch-buffer))
-(global-set-key (kbd "<M-f3>") (toggle-minibuffer 'eyebrowse-modify-buffer-config))
-(global-set-key (kbd "<C-f3>") 'eyebrowse-switch-other-buffer)
+ ;; eyebrowse buffer config
+ (global-set-key (kbd "<f3>") (toggle-minibuffer 'eyebrowse-switch-buffer))
+ (global-set-key (kbd "<M-f3>") (toggle-minibuffer 'eyebrowse-modify-buffer-config))
+ (global-set-key (kbd "<C-f3>") 'eyebrowse-switch-other-buffer))
 
 ;; customized dir
 (global-set-key (kbd "<f4>") (toggle-minibuffer 'switch-to-customized-dir))
 ;; *** not implemented ***
 (global-set-key (kbd "<C-f4>") 'cusomized-dir-action)
-
-;; eyebrowse config
-(global-set-key (kbd "<f5>") (toggle-minibuffer 'eyebrowse-list-configs))
-(global-set-key (kbd "<M-f5>") (toggle-minibuffer 'eyebrowse-modify-config))
-(global-set-key (kbd "<C-f5>") 'eyebrowse-last-window-config)
 
 ;; TODO: move to editors.el
 ;; (global-set-key (kbd "<f5>") '(lambda () (interactive)
