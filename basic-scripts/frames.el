04a3f30655a13febcd3ca503f5c16482efd1b934
gadmyth <gadmyth@gmail.com>
2022-10-10 22:17:39 +0800

    frames: add function to resize and move frame

--- basic-scripts/frames.el
+++ basic-scripts/frames.el
@@ -34,6 +34,158 @@
     (set-frame-parameter frame 'fullscreen nil)
     (set-suitable-frame-size-inner (selected-frame))))
 
+(defun scale-frame-size (frame scale)
+  "FRAME: , SCALE."
+  (set-frame-size
+   frame
+   (round (* (frame-parameter frame 'width) scale))
+   (round (* (frame-parameter frame 'height) scale))))
+
+(defun adjust-frame-horizontally (frame delta)
+  "FRAME: , DELTA."
+  (set-frame-width
+   frame
+   (+ (frame-parameter frame 'width) delta)))
+
+(defun adjust-frame-vertically (frame delta)
+  "FRAME: , DELTA."
+  (set-frame-height
+   frame
+   (+ (frame-parameter frame 'height) delta)))
+
+(defun scale-frame-horizontally (frame scale)
+  "FRAME: , SCALE."
+  (set-frame-width
+   frame
+   (round (* (frame-parameter frame 'width) scale))))
+
+(defun scale-frame-vertically (frame scale)
+  "FRAME: , SCALE."
+  (set-frame-height
+   frame
+   (round (* (frame-parameter frame 'height) scale))))
+
+(defun adjust-frame-size ()
+  "."
+  (interactive)
+  (let* ((ev last-command-event)
+         (modifiers (event-modifiers ev))
+         (scale-p (memq 'shift modifiers))
+         (base (event-basic-type ev))
+         (frame (selected-frame)))
+    (cond
+     (scale-p
+      (pcase base
+        ('right (scale-frame-horizontally frame 2))
+        ('left (scale-frame-horizontally frame 0.5))
+        ('down (scale-frame-vertically frame 2))
+        ('up (scale-frame-vertically frame 0.5))
+        (_ nil)))
+     (t
+      (pcase base
+        ('right (adjust-frame-horizontally frame 5))
+        ('left (adjust-frame-horizontally frame -5))
+        ('down (adjust-frame-vertically frame 5))
+        ('up (adjust-frame-vertically frame -5))
+        (_ nil))))
+    (message "use ctrl-left, ctrl-right, ctrl-up, ctrl-down for further adjustment")
+    (set-transient-map
+     (let ((map (make-sparse-keymap)))
+       (define-key map (vector (append '(control) (list 'left)))
+         #'adjust-frame-size)
+       (define-key map (vector (append '(control) (list 'right)))
+         #'adjust-frame-size)
+       (define-key map (vector (append '(control) (list 'up)))
+         #'adjust-frame-size)
+       (define-key map (vector (append '(control) (list 'down)))
+         #'adjust-frame-size)
+       (define-key map (vector (append '(control shift) (list 'left)))
+         #'adjust-frame-size)
+       (define-key map (vector (append '(control shift) (list 'right)))
+         #'adjust-frame-size)
+       (define-key map (vector (append '(control shift) (list 'up)))
+         #'adjust-frame-size)
+       (define-key map (vector (append '(control shift) (list 'down)))
+         #'adjust-frame-size)
+       map))))
+
+(defun move-frame-horizontally (frame delta)
+  "FRAME: , DELTA."
+  (set-frame-parameter
+   frame
+   'left
+   (+ (frame-parameter frame 'left) delta)))
+
+(defun move-frame-vertically (frame delta)
+  "FRAME: , DELTA."
+  (set-frame-parameter
+   frame
+   'top
+   (+ (frame-parameter frame 'top) delta)))
+
+(defun adjust-frame-position ()
+  "."
+  (interactive)
+  (let* ((ev last-command-event)
+         (modifiers (event-modifiers ev))
+         (scale-p (memq 'shift modifiers))
+         (base (event-basic-type ev))
+         (frame (selected-frame)))
+    (cond
+     (scale-p
+      (pcase base
+        ('right (move-frame-horizontally frame 20))
+        ('left (move-frame-horizontally frame -20))
+        ('down (move-frame-vertically frame 20))
+        ('up (move-frame-vertically frame -20))
+        (_ nil)))
+     (t
+      (pcase base
+        ('right (move-frame-horizontally frame 5))
+        ('left (move-frame-horizontally frame -5))
+        ('down (move-frame-vertically frame 5))
+        ('up (move-frame-vertically frame -5))
+        (_ nil))))
+    (message "use meta-left, meta-right, meta-up, meta-down for further adjustment")
+    (set-transient-map
+     (let ((map (make-sparse-keymap)))
+       (define-key map (vector (append '(meta) (list 'left)))
+         #'adjust-frame-position)
+       (define-key map (vector (append '(meta) (list 'right)))
+         #'adjust-frame-position)
+       (define-key map (vector (append '(meta) (list 'up)))
+         #'adjust-frame-position)
+       (define-key map (vector (append '(meta) (list 'down)))
+         #'adjust-frame-position)
+       (define-key map (vector (append '(meta shift) (list 'left)))
+         #'adjust-frame-position)
+       (define-key map (vector (append '(meta shift) (list 'right)))
+         #'adjust-frame-position)
+       (define-key map (vector (append '(meta shift) (list 'up)))
+         #'adjust-frame-position)
+       (define-key map (vector (append '(meta shift) (list 'down)))
+         #'adjust-frame-position)
+       map))))
+
+(global-set-key (kbd "C-x C-<left>") 'adjust-frame-size)
+(global-set-key (kbd "C-x C-<right>") 'adjust-frame-size)
+(global-set-key (kbd "C-x C-<up>") 'adjust-frame-size)
+(global-set-key (kbd "C-x C-<down>") 'adjust-frame-size)
+(global-set-key (kbd "C-x C-S-<left>") 'adjust-frame-size)
+(global-set-key (kbd "C-x C-S-<right>") 'adjust-frame-size)
+(global-set-key (kbd "C-x C-S-<up>") 'adjust-frame-size)
+(global-set-key (kbd "C-x C-S-<down>") 'adjust-frame-size)
+
+(global-set-key (kbd "C-x M-<left>") 'adjust-frame-position)
+(global-set-key (kbd "C-x M-<right>") 'adjust-frame-position)
+(global-set-key (kbd "C-x M-<up>") 'adjust-frame-position)
+(global-set-key (kbd "C-x M-<down>") 'adjust-frame-position)
+(global-set-key (kbd "C-x M-S-<left>") 'adjust-frame-position)
+(global-set-key (kbd "C-x M-S-<right>") 'adjust-frame-position)
+(global-set-key (kbd "C-x M-S-<up>") 'adjust-frame-position)
+(global-set-key (kbd "C-x M-S-<down>") 'adjust-frame-position)
+
+
 (if (boundp 'tool-bar-mode) (tool-bar-mode (bound-or-default *tool-bar-mode* t)))
 (if (boundp 'menu-bar-mode) (menu-bar-mode (bound-or-default *menu-bar-mode* t)))
 (if (boundp 'blink-cursor-mode) (blink-cursor-mode (bound-or-default *blink-cursor-mode* t)))
