14818f1e1528372c72d005f51ee21b154efba8ea
gadmyth <gadmyth@gmail.com>
2023-10-16 11:03:21 +0800

    mybatis: refactor mybatis.el, create mybatis mapper xml file from template; expand mybatis insert and update directly inside xml file

--- el-extends/mybatis.el
+++ el-extends/mybatis.el
@@ -8,7 +8,7 @@
 (require 'yasnippet)
 
 
-(defun parse-mybatis-result-line (prop)
+(defun mybatis-parse-result-line (prop)
   "Parse mybatis result line of PROP property."
   (let ((regexp (format "^.*?<result\s*.*%s=\"\\(.*?\\)\"" prop))
         (line (buffer-substring (line-beginning-position) (line-end-position))))
@@ -17,22 +17,22 @@
           ;; (message "parsed %s is %s" prop str)
           str))))
 
-(defun parse-mybatis-result-property-string ()
+(defun mybatis-parse-result-property-string ()
   "."
   (interactive)
-  (parse-mybatis-result-line "property"))
+  (mybatis-parse-result-line "property"))
 
-(defun parse-mybatis-result-jdbc-type-string ()
+(defun mybatis-parse-result-jdbc-type-string ()
   "."
   (interactive)
-  (parse-mybatis-result-line "jdbcType"))
+  (mybatis-parse-result-line "jdbcType"))
 
-(defun parse-mybatis-result-column-string ()
+(defun mybatis-parse-result-column-string ()
   "."
   (interactive)
-  (parse-mybatis-result-line "column"))
+  (mybatis-parse-result-line "column"))
 
-(defun parse-mybatis-result-properties ()
+(defun mybatis-parse-result-properties ()
   "."
   (interactive)
   (when (region-active-p)
@@ -42,11 +42,11 @@
             (list))
         (goto-char start)
         (while (< (point) end)
-          (push (parse-mybatis-result-property-string) list)
+          (push (mybatis-parse-result-property-string) list)
           (forward-line 1))
         (reverse list)))))
 
-(defun parse-mybatis-result-columns ()
+(defun mybatis-parse-result-columns ()
   "."
   (interactive)
   (when (region-active-p)
@@ -56,7 +56,7 @@
             (list))
         (goto-char start)
         (while (< (point) end)
-          (push (parse-mybatis-result-column-string) list)
+          (push (mybatis-parse-result-column-string) list)
           (forward-line 1))
         (reverse list)))))
 
@@ -64,124 +64,169 @@
   "."
   (interactive)
   (message "<sql id=\"Base_Column_List\">")
-  (let ((column-list (s-join "," (parse-mybatis-result-columns))))
+  (let ((column-list (s-join "," (mybatis-parse-result-columns))))
     (message column-list))
   (message "</sql>"))
 
-(defun generate-mybatis-insert-column (column)
-  "."
-  (let* ((property (s-lower-camel-case column))
-         (str (format "<if test=\"%s != null\">\n\t%s,\n</if>"
-                      property
-                      column)))
-    (message str)))
+(defun mybatis-generate-insert-column (column)
+  "COLUMN."
+  (interactive)
+  (let ((property (s-lower-camel-case column)))
+    (message "property: %S, column: %S" property column)
+    (yas-expand-snippet-with-params "mybatis-if-column-block" property column)))
 
-(defun generate-mybatis-insert-value (column jdbc-type)
-  "."
-  (let* ((property (s-lower-camel-case column))
-         (str (format "<if test=\"%s != null\">\n\t#{%s,jdbcType=%s},\n</if>"
-                     property
-                     property
-                     jdbc-type)))
-    (message str)))
+(defun mybatis-generate-insert-value (column jdbc-type)
+  "COLUMN, JDBC-TYPE."
+  (let* ((property (s-lower-camel-case column)))
+    (yas-expand-snippet-with-params "mybatis-if-column-value-block" property jdbc-type)))
 
-(defun generate-mybatis-update-value (column jdbc-type)
-  "."
-  (let* ((property (s-lower-camel-case column))
-         (str (format "<if test=\"%s != null\">\n\t%s = #{%s,jdbcType=%s},\n</if>"
-                      property
-                      column
-                      property
-                      jdbc-type)))
-    (message str)))
+(defun mybatis-generate-update-value (column jdbc-type)
+  "COLUMN, JDBC-TYPE."
+  (let* ((property (s-lower-camel-case column)))
+    (yas-expand-snippet-with-params "mybatis-update-column-value-block" property column jdbc-type)))
 
-(defun generate-mybatis-insert-columns ()
+(defun mybatis-generate-insert-columns ()
   "."
   (interactive)
-  (when (region-active-p)
+  (let ((list))
     (save-excursion
-      (let ((start (region-beginning))
-            (end (region-end))
-            (list))
+      (goto-char 0)
+      (let ((start (progn (search-forward "<resultMap" nil t 1)
+                          (point)))
+            (end (progn (search-forward "</resultMap>" nil t 1)
+                        (point))))
         (goto-char start)
         (while (< (point) end)
-          (let* ((column (parse-mybatis-result-column-string)))
+          (forward-line 1)
+          (let* ((column (mybatis-parse-result-column-string)))
+            (message "insert column: %S" column)
             (when column
-              (push (generate-mybatis-insert-column column) list))
-            (forward-line 1))
-          (reverse list))))))
+              (push column list))))))
+    ;; insert column
+    (message "columns: %S" list)
+    (let ((len (length list))
+          (i 0))
+      (dolist (column (reverse list))
+        (mybatis-generate-insert-column column)
+        (when (< i (- len 1))
+          (newline-and-indent)
+          (setq i (+ i 1)))))))
 
-(defun parse-mybatis-result-map ()
+(defun mybatis-parse-result-map ()
   "."
-  (when (region-active-p)
-    (save-excursion
-      (let ((start (region-beginning))
-            (end (region-end))
-            list
-            found)
-        (goto-char start)
-        (while (and (not found)
-                    (< (point) end))
-          (let ((regexp "^.*?<resultMap\s*id=\"\\(.*?\\)\"\s*type=\"\\(.*?\\)\"")
-                (line (buffer-substring (line-beginning-position) (line-end-position))))
-            (if (string-match regexp line)
-                (let ((map-id (substring-no-properties line (match-beginning 1) (match-end 1)))
-                      (map-type (substring-no-properties line (match-beginning 2) (match-end 2))))
-                  ;; (message "parsed resultMap: map-id: %s, map-type: %s" map-id map-type)
-                  (setq found `(,map-id ,map-type)))
-              (forward-line))))
-        found))))
+  (save-excursion
+    (goto-char 0)
+    (let* ((str "<resultMap")
+           (start (progn (search-forward str nil t 1)
+                         (point)))
+           (end (progn (search-forward "</resultMap>" nil t 1)
+                       (point)))
+           list
+           found)
+      (goto-char (- start (length str)))
+      (while (and (not found)
+                  (< (point) end))
+        (let ((regexp "^.*?<resultMap\s*id=\"\\(.*?\\)\"\s*type=\"\\(.*?\\)\"")
+              (line (buffer-substring (line-beginning-position) (line-end-position))))
+          (if (string-match regexp line)
+              (let ((map-id (substring-no-properties line (match-beginning 1) (match-end 1)))
+                    (map-type (substring-no-properties line (match-beginning 2) (match-end 2))))
+                ;; (message "parsed resultMap: map-id: %s, map-type: %s" map-id map-type)
+                (setq found `(,map-id ,map-type)))
+            (forward-line))))
+      found)))
 
-(defun generate-mybatis-insert-values ()
-  "."
-  (interactive)
-  (when (region-active-p)
-    (save-excursion
-      (let ((start (region-beginning))
-            (end (region-end))
-            (list))
-        (goto-char start)
-        (while (< (point) end)
-          (let* ((column (parse-mybatis-result-column-string))
-                 (jdbc-type (parse-mybatis-result-jdbc-type-string)))
-            (when (and column jdbc-type)
-              (push (generate-mybatis-insert-value column jdbc-type) list))
-            (forward-line 1))
-          (reverse list))))))
-
-(defun generate-mybatis-update-values ()
+(defun mybatis-generate-insert-columns ()
   "."
   (interactive)
-  (when (region-active-p)
+  (let ((list))
     (save-excursion
-      (let ((start (region-beginning))
-            (end (region-end))
-            (list))
+      (goto-char 0)
+      (let ((start (progn (search-forward "<resultMap" nil t 1)
+                          (point)))
+            (end (progn (search-forward "</resultMap>" nil t 1)
+                        (point))))
         (goto-char start)
         (while (< (point) end)
-          (let* ((column (parse-mybatis-result-column-string))
-                 (jdbc-type (parse-mybatis-result-jdbc-type-string)))
+          (forward-line 1)
+          (let* ((column (mybatis-parse-result-column-string)))
+            (when column
+              (push column list))))))
+    ;; insert column
+    (dolist (column (reverse list))
+      (mybatis-generate-insert-column column)
+      (indent-according-to-mode))))
+
+(defun mybatis-generate-insert-values ()
+  "."
+  (interactive)
+  (let ((list)
+        (type-list))
+    (save-excursion
+      (goto-char 0)
+      (let ((start (progn (search-forward "<resultMap" nil t 1)
+                          (point)))
+            (end (progn (search-forward "</resultMap>" nil t 1)
+                        (point))))
+        (goto-char start)
+        (while (< (point) end)
+          (forward-line 1)
+          (let* ((column (mybatis-parse-result-column-string))
+                 (jdbc-type (mybatis-parse-result-jdbc-type-string)))
             (when (and column jdbc-type)
-              (push (generate-mybatis-update-value column jdbc-type) list))
-            (forward-line 1))
-          (reverse list))))))
+              (push (cons column jdbc-type) list))))))
+    ;; insert column and type
+    (let ((len (length list))
+          (i 0))
+      (dolist (pair (reverse list))
+        (mybatis-generate-insert-value (car pair) (cdr pair))
+        (when (< i (- len 1))
+          (newline-and-indent)
+          (setq i (+ i 1)))))))
+
+(defun mybatis-generate-update-values ()
+  "."
+  (interactive)
+  (let ((list)
+        (type-list))
+    (save-excursion
+      (goto-char 0)
+      (let ((start (progn (search-forward "<resultMap" nil t 1)
+                          (point)))
+            (end (progn (search-forward "</resultMap>" nil t 1)
+                        (point))))
+        (goto-char start)
+        (while (< (point) end)
+          (forward-line 1)
+          (let* ((column (mybatis-parse-result-column-string))
+                 (jdbc-type (mybatis-parse-result-jdbc-type-string)))
+            (when (and column jdbc-type)
+              (push (cons column jdbc-type) list))))))
+    ;; insert column and type
+    (let ((len (length list))
+          (i 0))
+      (dolist (pair (reverse list))
+        (mybatis-generate-update-value (car pair) (cdr pair))
+        (when (< i (- len 1))
+          (newline-and-indent)
+          (setq i (+ i 1)))))))
 
 (defun generate-sql-column-definitions ()
-  "."
-  (interactive)
-  (when (region-active-p)
-    (save-excursion
-      (let ((start (region-beginning))
-            (end (region-end))
-            (list))
-        (goto-char start)
-        (while (< (point) end)
-          (let* ((column (parse-mybatis-result-column-string))
-                 (jdbc-type (parse-mybatis-result-jdbc-type-string)))
-            (when (and column jdbc-type)
-              (push (generate-sql-column-definition column jdbc-type) list))
-            (forward-line 1))
-          (reverse list))))))
+    "."
+    (interactive)
+    (when (region-active-p)
+      (save-excursion
+        (let ((start (region-beginning))
+              (end (region-end))
+              (list))
+          (goto-char start)
+          (while (< (point) end)
+            (let* ((column (mybatis-parse-result-column-string))
+                   (jdbc-type (mybatis-parse-result-jdbc-type-string)))
+              (when (and column jdbc-type)
+                (push (generate-sql-column-definition column jdbc-type) list))
+              (forward-line 1))
+            (reverse list))))))
 
 (defun generate-sql-column-definition (column jdbc-type)
   "."
@@ -190,31 +235,27 @@
                      jdbc-type)))
     (message str)))
 
-(defun generate-mybatis-insert-block ()
+(defun mybatis-generate-insert-block ()
   "."
   (interactive)
-  (let ((table-name (read-string "Please input table name: ")))
-    (message "<insert id=\"insert\" parameterType=\"%s\">" (cadr (parse-mybatis-result-map)))
-    (message "insert into %s" table-name)
-    (message "<trim prefix=\"(\" suffix=\")\" suffixOverrides=\",\">")
-    (generate-mybatis-insert-columns)
-    (message "</trim>")
-    (message "<trim prefix=\"values (\" suffix=\")\" suffixOverrides=\",\">")
-    (generate-mybatis-insert-values)
-    (message "</trim>")
-    (message "</insert>")))
+  (let ((table-name (read-string "Please input table name: "))
+        (parameter-type (cadr (mybatis-parse-result-map))))
+    (yas-expand-snippet-with-params "mybatis-insert-block" parameter-type table-name)
+    (yas-expand-snippet-with-params "mybatis-trim-block")
+    (mybatis-generate-insert-columns)
+    (forward-line)
+    (end-of-line)
+    (newline-and-indent)
+    (yas-expand-snippet-with-params "mybatis-values-trim-block")
+    (mybatis-generate-insert-values)))
 
-(defun generate-mybatis-update-block ()
+(defun mybatis-generate-update-block ()
   "."
   (interactive)
-  (let ((table-name (read-string "Please input table name: ")))
-    (message "<update id=\"update\" parameterType=\"%s\">" (cadr (parse-mybatis-result-map)))
-    (message "update %s" table-name)
-    (message "<set>")
-    (generate-mybatis-update-values)
-    (message "</set>")
-    (message "where id = #{id,jdbcType=BIGINT}")
-    (message "</update>")))
+  (let ((table-name (read-string "Please input table name: "))
+        (parameter-type (cadr (mybatis-parse-result-map))))
+    (yas-expand-snippet-with-params "mybatis-update-block" parameter-type table-name)
+    (mybatis-generate-update-values)))
 
 (defun generate-create-table-sql ()
   "."
@@ -228,7 +269,7 @@
 (defun create-java-property-from-mybatis-column ()
   "."
   (interactive)
-  (let* ((prop (parse-mybatis-column-property-string))
+  (let* ((prop (mybatis-parse-column-property-string))
          (java-property (format "private String %s;" prop)))
     (kill-new java-property)))
 
@@ -246,9 +287,10 @@
            (setq jdbc-type (pcase type
                              ("String" "VARCHAR")
                              ("Integer" "INTEGER")
-                             ("Date" "VARCHAR")
+                             ("Date" "TIMESTAMP")
                              ("int" "INTEGER")
-                             ("long" "INTEGER")
+                             ("long" "BIGINT")
+                             ("Long" "BIGINT")
                              (_ (error "Can't find jdbcType for java type %s" type))))
            (setq column (s-snake-case property))
            `((property . ,property)
@@ -270,17 +312,30 @@
           (message "full-class-name: %S" full-class-name)
           full-class-name)))))
 
-(defun generate-mybatis-result-map-from-java-class ()
+(defun mybatis-create-result-map-from-java-class ()
   "."
   (interactive)
-  (let ((full-class-name (parse-full-java-class-name)))
-    (message "<resultMap id=\"BaseResultMap\" type=\"%s\">" full-class-name))
-  (seq-doseq (prop-pair (parse-properties-from-java-class))
-    (message "<result column=\"%s\" jdbcType=\"%s\" property=\"%s\"/>"
-             (assoc-default 'column prop-pair)
-             (assoc-default 'jdbc-type prop-pair)
-             (assoc-default 'property prop-pair)))
-  (message "</resultMap>"))
+  (when-let ((project-root (textmate-find-project-root)))
+    (let* ((default-directory project-root)
+           (class-name (read-string "Please input the java class name: "))
+           (cmd (format "cd %s; find . -name %s.java" default-directory class-name))
+           (file-names (string-split (shell-command-to-string cmd)))
+           (file-name (concat default-directory
+                              "/"
+                              (completing-read "Please select a file: " file-names nil t)))
+           (full-class-name (with-temp-buffer
+                              (insert-file-contents file-name)
+                              (parse-full-java-class-name))))
+      (yas-expand-snippet-with-params "mybatis-mapper-result-map" full-class-name)
+      (seq-doseq (prop-pair (with-temp-buffer
+                              (insert-file-contents file-name)
+                              (parse-properties-from-java-class)))
+        (yas-expand-snippet-with-params
+         "mybatis-result-column"
+         (assoc-default 'column prop-pair)
+         (assoc-default 'jdbc-type prop-pair)
+         (assoc-default 'property prop-pair))
+        (newline-and-indent)))))
 
 (defun mybatis-create-mapper-java-file ()
   "."
@@ -294,7 +349,7 @@
       (when (> (length directories) 0)
         (let* ((directory (completing-read "Please select a directory: " directories nil t))
                (file-name (read-string "Please input the mapper java file name: "))
-               (full-file-name (format "%s/%s/%s" default-directory directory file-name))
+               (full-file-name (format "%s/%s/%s.java" default-directory directory file-name))
                package-head)
           (let ((cmd (format "cd %s; cd %s; egrep 'package .*;' $(ls | head -n 1)" default-directory directory)))
             (setq package-head (shell-command-to-string cmd)))
@@ -322,11 +377,11 @@
       (when (> (length directories) 0)
         (let* ((directory (completing-read "Please select a directory: " directories nil t))
                (file-name (read-string "Please input the mapper xml file name: "))
-               (full-file-name (format "%s/%s/%s" default-directory directory file-name))
+               (full-file-name (format "%s/%s/%s.xml" default-directory directory file-name))
                (mapper-java-class ""))
           ;; parse mapper-java-class
           (let* ((mapper-java-class-name (read-string "Please input mapper java class name: "))
-                 (cmd (format "cd %s; find . -name %s -exec realpath {} +" default-directory mapper-java-class-name))
+                 (cmd (format "cd %s; find . -name %s.java -exec realpath {} +" default-directory mapper-java-class-name))
                  (mapper-java-class-full-name (replace-regexp-in-string "\n" "" (shell-command-to-string cmd))))
             (message "mapper-java-class-full-name: %s" mapper-java-class-full-name)
             (message "exists: %S" (file-exists-p mapper-java-class-full-name))
