1bbea280b59184fb0570e9fc61f8a12eda8202d9
gadmyth <gadmyth@gmail.com>
2024-07-28 13:55:05 +0800

    init file: completing-read add space

--- init-scripts/emacs.init.el
+++ init-scripts/emacs.init.el
@@ -13,7 +13,7 @@
 
 (let* ((init-files '(".emacs.frame.el" ".emacs.shell.el" ".emacs.simple.el"))
        (default-file ".emacs.frame.el")
-       (selected (completing-read "Please select the init file:" init-files nil t nil nil default-file)))
+       (selected (completing-read "Please select the init file: " init-files nil t nil nil default-file)))
   (load-file (expand-file-name selected +emacs-init-script-directory+)))
 
 (provide 'emacs.init)
