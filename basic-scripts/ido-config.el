f33bf13fd77ff01d0f22dfb5732491bed5645b34
gadmyth <gadmyth@gmail.com>
2024-08-11 09:20:48 +0800

    ido-config: toggle of ido-everywhere, ido-enable-flex-matching, ido-ubiquitous-mode to speed up

--- basic-scripts/ido-config.el
+++ basic-scripts/ido-config.el
@@ -3,11 +3,11 @@
 ;;; Code:
 
 (ido-mode t)
-(ido-everywhere t)
-(setq ido-enable-flex-matching t)
+;; (ido-everywhere t)
+;; (setq ido-enable-flex-matching t)
 
-(require 'ido-completing-read+)
-(ido-ubiquitous-mode 1)
+;; (require 'ido-completing-read+)
+;; (ido-ubiquitous-mode 1)
 
 (provide 'ido-config)
 ;;; ido-config.el ends here
