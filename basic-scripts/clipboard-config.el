439ed51dfbf80139159ce28cc6c4bac99d3b1858
gadmyth <gadmyth@gmail.com>
2025-05-18 17:19:22 +0800

    clipboard: don't sync clipboard to system when is killed

--- basic-scripts/clipboard-config.el
+++ basic-scripts/clipboard-config.el
@@ -4,6 +4,7 @@
 
 (when (eq window-system 'x)
   (setq select-enable-clipboard t)
+  (setq x-select-enable-clipboard-manager nil)
   (setq interprogram-paste-function 'x-selection-value)
   (setq x-stretch-cursor t)
   )
